d6d64dc668edd2d5bfaee4b06d84f2e749032337
Muhammad Idham Ramadhan <muhammad.idham@veritrans.co.id>
2015-06-23 15:38:04 +0700

    Add dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,10 @@ class PagesController < ApplicationController
 before_action :authenticate_user!
 
   def index
+  	@recovered = IncidentReport.where(current_status: 'Recovered').count
+  	@ongoing = IncidentReport.where(current_status: 'Ongoing').count
+  	@implemented = IncidentReport.where(measurer_status: 'Implemented').count
+  	@development = IncidentReport.where(measurer_status: 'Development').count
   end
 
 end
