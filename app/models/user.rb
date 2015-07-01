62d9261062ac0e94f7c1cfd0da016e25ce10a2cd
Muhammad Idham Ramadhan <muhammad.idham@veritrans.co.id>
2015-07-01 10:51:36 +0700

    auth google

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,8 +23,8 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(auth)
     where(provider: auth[:provider], uid: auth[:uid]).first_or_create do |user|
-      #user.name = auth[:info][:name]
       user.email = auth[:info][:email]
+      user.name = auth[:info][:name]
       user.role = 'user'
       user.locked_at = Time.current
     end
