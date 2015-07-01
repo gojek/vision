6b3a6b3f32362d3d43a619c800b8acd6713ed9e6
Muhammad Idham Ramadhan <muhammad.idham@veritrans.co.id>
2015-07-01 10:15:31 +0700

    add separate api_keys for development and production

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,7 @@
 # 'config/initializers/omniauth.rb'
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '***REMOVED***', '***REMOVED***', {
+  API_KEYS = YAML::load_file("#{Rails.root}/config/api_key.yml")[Rails.env]
+  provider :google_oauth2, API_KEYS['google']['api_key'], API_KEYS['google']['api_secret'], {
   	:prompt => '',
   	:hd => '***REMOVED***'
   }
