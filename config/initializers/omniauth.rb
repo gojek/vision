fa4a5b5f3ec8c0e06e6dbee7a0fd4f42d8c4a11e
Muhammad Idham Ramadhan <muhammad.idham@veritrans.co.id>
2015-07-01 10:25:29 +0700

    auth google

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,6 @@
 # 'config/initializers/omniauth.rb'
 Rails.application.config.middleware.use OmniAuth::Builder do
-  API_KEYS = YAML::load_file("#{Rails.root}/config/api_key.yml")[Rails.env]
-  provider :google_oauth2, API_KEYS['google']['api_key'], API_KEYS['google']['api_secret'], {
+  provider :google_oauth2, '***REMOVED***', '***REMOVED***', {
   	:prompt => '',
   	:hd => '***REMOVED***'
   }
