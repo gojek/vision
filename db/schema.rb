6e8ce201fb7327618edf8305d6fb404295ba29e7
Muhammad Idham Ramadhan <muhammad.idham@veritrans.co.id>
2015-07-06 11:52:50 +0700

    user role

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150706035158) do
+ActiveRecord::Schema.define(version: 20150706044556) do
 
   create_table "approvers", force: :cascade do |t|
     t.string   "name"
@@ -169,6 +169,7 @@ ActiveRecord::Schema.define(version: 20150706035158) do
     t.string   "name"
     t.string   "uid"
     t.string   "provider"
+    t.boolean  "is_admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
