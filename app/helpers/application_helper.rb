6dbc3adb95624ea6263ca0b78c619fb06befb356
Muhammad Idham Ramadhan <muhammad.idham@veritrans.co.id>
2015-07-08 15:30:32 +0700

    fixing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
 	def is_release_manager
 		current_user.role == "release_manager"
 	end
+	def count
+    	Approver.where(approve: nil).where(user_id: current_user.id).count
+  	end
+  	def is_approver
+  		current_user.role == "approver"
+  	end
 end
--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,18 @@ module Vision
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Jakarta'
+    config.generators do |g|
+        g.test_framework :rspec,
+            fixtures: true,
+            view_specs: false,
+            helper_specs: false,
+            routing_specs:false,
+            controller_specs: true,
+            request_spec: false
+        g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
+
+
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
