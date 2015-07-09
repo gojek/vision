d3d7c57ac6c9dd7fa0df38c506d454f73af98389
Muhammad Idham Ramadhan <muhammad.idham@veritrans.co.id>
2015-07-09 18:10:31 +0700

    refactoring change request status

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150709043935) do
+ActiveRecord::Schema.define(version: 20150709062113) do
 
   create_table "approvers", force: :cascade do |t|
     t.string   "name"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20150709043935) do
     t.text     "object"
     t.datetime "created_at"
     t.string   "author_username"
+    t.text     "object_changes"
   end
 
   add_index "change_request_versions", ["item_type", "item_id"], name: "index_change_request_versions_on_item_type_and_item_id"
