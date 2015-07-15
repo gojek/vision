841379b6a4714f9cee1964490f8d3bc771cf5345
Muhammad Idham Ramadhan <muhammad.idham@veritrans.co.id>
2015-07-15 13:55:51 +0700

    CAB Date validation

--- app/models/cab.rb
+++ app/models/cab.rb
@@ -1,4 +1,9 @@
 class Cab < ActiveRecord::Base
 	has_many :change_requests, dependent: :nullify
 	validates :meet_date, presence:true, uniqueness:true
+	validate :validate_meet_date
+
+	def validate_meet_date
+		errors.add("Meet date", "is invalid.") unless meet_date > Time.now
+	end
 end
