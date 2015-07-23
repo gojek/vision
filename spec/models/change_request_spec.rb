c93ebbbb2ad37ac4b87fd5e18590c03360d5fed9
Muhammad Idham Ramadhan <muhammad.idham@veritrans.co.id>
2015-07-23 16:15:56 +0700

    add cab, change_request_status rspec and modify user, change_request rspec test

--- spec/models/change_request_spec.rb
+++ spec/models/change_request_spec.rb
@@ -1,4 +1,19 @@
 require 'spec_helper'
 
 describe ChangeRequest do
+	it "initial state is submitted when first created" do
+		change_request = FactoryGirl.build(:change_request)
+		expect(change_request.aasm_state).to eq "submitted"
+	end
+
+	it "wont be approvable when first created" do
+		change_request = FactoryGirl.build(:change_request)
+		expect(change_request.approvable?).to eq false
+	end
+
+	it "approvers_count will return the count of approvers" do
+		change_request = FactoryGirl.build(:change_request)
+		expect(change_request.approvers_count).to eq 0
+	end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,66 +2,69 @@ require 'spec_helper'
 
 describe User do
 	it "is valid with one of these role : approver, requestor, release_manager" do
-		user = User.new(
-			email: 'patrick@veritrans.co.id',
-			role: 'approver')
-		other_user = User.new(
-			email: 'patrick@veritrans.co.id',
-			role: 'requestor')
-		another_user = User.new(
-			email: 'patrick@veritrans.co.id',
-			role: 'release_manager')
+		user = FactoryGirl.build(:user)
+		other_user = FactoryGirl.build(:user, role: 'approver')
+		another_user = FactoryGirl.build(:user, role: 'release_manager')
 		expect(user).to be_valid
 		expect(other_user).to be_valid
 		expect(another_user).to be_valid
 	end
 	it "is invalid with role other than approver, requestor, release_manager" do
-		user = User.new(
-			email: 'patrick@veritrans.co.id',
-			role: 'jagoan')
+		user = FactoryGirl.build(:user, role: 'jagoan')
 		expect(user).to have(1).errors_on(:role)
 	end
 	it "is valid with a veritrans email" do 
-		user = User.new(
-			email: 'patrick@veritrans.co.id',
-			role: 'requestor')
+		user = FactoryGirl.build(:user)
 		expect(user).to be_valid
 	end
 	it "is invalid without an email address" do
-		user = User.new(
-			email: nil,
-			role: 'requestor')
+		user = FactoryGirl.build(:user, email: nil)
 		expect(user).to have(2).errors_on(:email)
 	end
 	it "is invalid with a duplicate email address" do
-		User.create(
-			email: 'patrick@veritrans.co.id',
-			role: 'requestor')
-		other_user = User.new(
-			email: 'patrick@veritrans.co.id',
-			role: 'requestor')
+		user = FactoryGirl.create(:user)
+		other_user = FactoryGirl.build(:user)
 		expect(other_user).to have(1).errors_on(:email)
 	end
 	it "is invalid with a non-veritrans email" do
-		user = User.new(
-			email: 'squidward@gmail.com',
-			role: 'requestor')
+		user = FactoryGirl.build(:user, email: 'squidward@gmail.com')
 		expect(user).to have(1).errors_on(:email)
 	end
 	it "returns true if account is not locked" do
-		user = User.create(
-			email: 'patrick@veritrans.co.id',
-			role: 'requestor',
-			locked_at: nil)
+		user = FactoryGirl.create(:user)
 		expect(user.account_active?).to eq true
 		expect(user.active_for_authentication?).to eq true
 	end
 	it "returns false if account is locked" do
-		user = User.create(
-			email: 'patrick@veritrans.co.id',
-			role: 'requestor',
-			locked_at: Time.now)
+		user = FactoryGirl.create(:user, locked_at: Time.now)
 		expect(user.account_active?).to eq false
 		expect(user.active_for_authentication?).to eq false
+	end
+
+	it "have find_version_author method that will return User who do the version" do
+		user = FactoryGirl.create(:user)
+		version = IncidentReportVersion.new(
+			item_type: 'IncidentReport',
+			item_id: 1,
+			event: 'update',
+			whodunnit: 1)
+		expect(User.find_version_author(version)).to eq user
+	end
+
+	it "will find the user based on the auth from omniauth if user already registered" do
+		user = FactoryGirl.create(:user)
+		auth = {:provider => 'google_oauth2', :uid => '123456'}
+		expect(User.from_omniauth(auth)).to eq user
+	end
+
+	it "will register new user based on the auth from omniauth if user not registered" do
+		auth = {:provider => 'google_oauth2', :uid => '123456', :info => {:email => 'patrick@veritrans.co.id', :name => 'patrick star'}}
+		user = User.from_omniauth(auth)
+		expect(user.provider).to eq auth[:provider]
+		expect(user.uid).to eq auth[:uid]
+		expect(user.email).to eq auth[:info][:email]
+		expect(user.name).to eq auth[:info][:name]
+		expect(user.role).to eq 'requestor'
+		expect(user.is_admin).to eq false
 	end
 end
