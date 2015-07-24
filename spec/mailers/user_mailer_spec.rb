843d3a0a3b7e49454d2135d74cb46058db6aa279
Muhammad Idham Ramadhan <muhammad.idham@veritrans.co.id>
2015-07-24 14:19:24 +0700

    add controller test for cab controller, and modify factory

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,5 +1,25 @@
 require "spec_helper"
 
 describe UserMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe ".notif_email(user, change_request, status)" do
+  	before do
+  		@user = FactoryGirl.create(:user)
+  		@change_request = FactoryGirl.create(:change_request)
+  		@status = FactoryGirl.create(:change_request_status)
+  		FactoryGirl.create(:user, email: 'squidward@***REMOVED***', id: 2, role: 'release_manager')
+  		FactoryGirl.create(:user, email: 'crab@***REMOVED***', id: 3, role: 'approver')
+  		@recipients = [@user.email, 'squidward@***REMOVED***', 'crab@***REMOVED***']
+  	end
+
+  	it "has appropriate subject" do
+      mail = UserMailer.notif_email(@user, @change_request, @status)
+      expect(mail.subject).to eq "Change Request Status Changed"
+    end
+
+    it "sent to all approvers" do
+      mail = UserMailer.notif_email(@user, @change_request, @status)
+      expect(mail.to).to eq @recipients
+    end
+  end
 end
