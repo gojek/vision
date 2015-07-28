85f58ad86972501154b776a6eab922845b48d379
Muhammad Idham Ramadhan <muhammad.idham@veritrans.co.id>
2015-07-28 14:45:01 +0700

    add definition of success&failure, add type and category field to ChangeRequest model

---
+++ db/migrate/20150728065633_add_attributes_to_change_request.rb
@@ -0,0 +1,17 @@
+class AddAttributesToChangeRequest < ActiveRecord::Migration
+  def change
+    add_column :change_requests, :definition_of_success, :text
+    add_column :change_requests, :definition_of_failed, :text
+    add_column :change_requests, :category_application, :boolean
+    add_column :change_requests, :category_network_equipment, :boolean
+    add_column :change_requests, :category_server, :boolean
+    add_column :change_requests, :category_user_access, :boolean
+    add_column :change_requests, :category_other, :string
+    add_column :change_requests, :type_security, :boolean
+    add_column :change_requests, :type_update, :boolean
+    add_column :change_requests, :type_install_uninstall, :boolean
+    add_column :change_requests, :type_configuration_change, :boolean
+    add_column :change_requests, :type_emergency_change, :boolean
+    add_column :change_requests, :type_other, :string
+  end
+end
