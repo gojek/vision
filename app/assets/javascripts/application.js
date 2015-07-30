99509563e7410a254d3e87e959d5d01593f06291
Muhammad Idham Ramadhan <muhammad.idham@veritrans.co.id>
2015-07-30 19:27:23 +0700

    remove row from textarea to make it responsive, add raw to incident report index

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,8 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+//= require Sortable
+//= require depends_on
 //= require jquery
 //= require jquery.turbolinks
 //= require jquery_ujs
