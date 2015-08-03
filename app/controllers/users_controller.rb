296fbcc640fda9040e539a29eb64820cf709f209
Muhammad Idham Ramadhan <muhammad.idham@veritrans.co.id>
2015-08-03 14:10:48 +0700

    add notice to user operation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find params[:id]
     if @user.update(update_user_params)
-      flash[:notice] = 'User updated succesfully'
+      flash[:user_notice] = 'User updated succesfully'
       redirect_to users_path
     else
       render action: 'edit'
@@ -27,21 +27,21 @@ class UsersController < ApplicationController
   def destroy
     @user = User.find params[:id]
     @user.destroy
-    flash[:notice] = 'User deleted succesfully'
+    flash[:user_notice] = 'User deleted succesfully'
     redirect_to users_path
   end
 
   def lock_user
     @user = User.find params[:id]
     @user.update_attribute(:locked_at, Time.current)
-    flash[:notice] = 'User locked succesfully'
+    flash[:user_notice] = 'User locked succesfully'
     redirect_to users_path
   end
 
   def unlock_user
     @user = User.find params[:id]
     @user.update_attribute(:locked_at, nil)
-    flash[:notice] = 'User unlocked succesfully'
+    flash[:user_notice] = 'User unlocked succesfully'
     redirect_to users_path
   end
 
