c1c9ea7c6e57b7abccc3fa797248b72de4dd4e84
Muhammad Idham Ramadhan <muhammad.idham@veritrans.co.id>
2015-08-03 13:48:20 +0700

    add signout button and modify signout routes

--- config/routes.rb
+++ config/routes.rb
@@ -55,7 +55,7 @@ Rails.application.routes.draw do
   devise_for :users, skip: [:sessions], controllers: { omniauth_callbacks: "users/omniauth_callbacks" } 
   devise_scope :user do
     get 'sign_in', :to => 'pages#signin', :as => :new_user_session
-    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   end 
   resources :pages
   resources :users
