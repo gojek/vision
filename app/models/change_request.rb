2c5a405722caea11c80d3209a43ab12e92379e74
Muhammad Idham Ramadhan <muhammad.idham@veritrans.co.id>
2015-08-04 13:24:03 +0700

    User Interface improvement, html truncate in index change request

--- app/models/change_request.rb
+++ app/models/change_request.rb
@@ -11,13 +11,16 @@ class ChangeRequest < ActiveRecord::Base
   acts_as_taggable
   has_paper_trail class_name: 'ChangeRequestVersion'
   SCOPE = %w(Major Minor)
-  validates :scope,
-            inclusion: { in: SCOPE, message: '%{ value } is not a valid scope' }
-  STATUS = %w(submitted scheduled rollbacked cancelled rejected deployed closed)
   PRIORITY = %w(Critical Urgent High Normal Low)
-  #validates :requestor_name, :requestor_position, :change_summary, :priority, :category, :cr_type, :change_requirement, :business_justification, :note, :analysis, :solution, :impact, :scope, :design,
-           # :backup, :testing_environment_avaible, :testing_procedure, :testing_notes, :schedule_change_date, :planned_completion, :grace_period_starts, :grace_period_end, :implementation_notes, :grace_period_notes,
-          #  :user_id, :net, :db, :os, presence: true
+  validates :scope,
+            inclusion: { in: SCOPE, message: '%{value} is not a valid scope' }
+  validates :priority,
+            inclusion: { in: PRIORITY, message: '%{value} is not a valid scope' }
+  STATUS = %w(submitted scheduled rollbacked cancelled rejected deployed closed)
+  
+  validates :requestor_name, :requestor_position, :change_summary, :priority,:change_requirement, :business_justification, :note, :analysis, :solution, :impact, :scope, :design,
+            :backup, :testing_environment_available, :testing_procedure, :testing_notes, :schedule_change_date, :planned_completion, :grace_period_starts, :grace_period_end, :implementation_notes, :grace_period_notes,
+            :user_id, :net, :db, :os, presence: true
   accepts_nested_attributes_for :implementers, :allow_destroy => true
   accepts_nested_attributes_for :testers, :allow_destroy => true
   accepts_nested_attributes_for :approvers, :allow_destroy => true
