1869a45808c14cc8e29b27d60f66789c245799b4
Muhammad Idham Ramadhan <muhammad.idham@veritrans.co.id>
2015-08-06 14:45:14 +0700

    Add token check, sign out user if has no token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :check_user
+  
+  def check_user
+  	token_required! unless current_user==nil
+  end
 
+  def token_required!
+  	sign_out current_user unless current_user.token != nil 
+  end 
+ 
 end
