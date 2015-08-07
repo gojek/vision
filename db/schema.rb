cdc3b6f6b216782eaf4c0f37aeb34fc4aee9e6a0
robeth <robethrahmatullah@gmail.com>
2015-08-07 14:01:09 +0700

    Add readme

--- db/schema.rb
+++ db/schema.rb
@@ -229,13 +229,12 @@ ActiveRecord::Schema.define(version: 20150805074251) do
   add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true
 
   create_table "versions", force: :cascade do |t|
-    t.string   "item_type",      null: false
-    t.integer  "item_id",        null: false
-    t.string   "event",          null: false
+    t.string   "item_type",  null: false
+    t.integer  "item_id",    null: false
+    t.string   "event",      null: false
     t.string   "whodunnit"
     t.text     "object"
     t.datetime "created_at"
-    t.text     "object_changes"
   end
 
   add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
