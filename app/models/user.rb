2dfd65b2ff9e0e8d0e3309bc1ec48c811a57ac98
Muhammad Idham Ramadhan <muhammad.idham@veritrans.co.id>
2015-08-11 12:16:08 +0700

    Comment and tidy up code

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,7 +31,6 @@ class User < ActiveRecord::Base
     1.day
   end
 
-
   def self.from_omniauth(auth)
     where(provider: auth[:provider], uid: auth[:uid]).first_or_create do |user|
       user.email = auth[:info][:email]
