2eac46da900e65bf3751bab955b6f603c336a7eb
Muhammad Idham Ramadhan <muhammad.idham@veritrans.co.id>
2015-08-19 13:40:41 +0700

    Able to edit CAB Agenda eventhough the time has passed; CAB google event will include all CR that will be discussed; CR validation time start and end deploy; Arrange Google Event when save change request deploy time from CAB Calendar; Add solving duration time in Incident Report so a statistic can be made from that

--- app/models/cab.rb
+++ app/models/cab.rb
@@ -2,7 +2,7 @@ class Cab < ActiveRecord::Base
   extend SimpleCalendar
   has_many :change_requests, dependent: :nullify
   validates :meet_date, presence:true, uniqueness:true
-  validate :validate_meet_date
+  validate :validate_meet_date, :on => :create
   PARTICIPANTS = %w(cr@veritrans.co.id it_operation@veritrans.co.id stig@veritrans.co.id)
   def validate_meet_date
     errors.add("Meet date", "is invalid.") unless meet_date!=nil && meet_date > Time.now
