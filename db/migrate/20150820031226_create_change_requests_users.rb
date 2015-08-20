0096fa463ffb35e17a3c7cb5e2326ef5b769895d
Muhammad Idham Ramadhan <muhammad.idham@veritrans.co.id>
2015-08-20 13:47:38 +0700

    add resolution duration and recovery duration & add collaborators to change request

---
+++ db/migrate/20150820031226_create_change_requests_users.rb
@@ -0,0 +1,8 @@
+class CreateChangeRequestsUsers < ActiveRecord::Migration
+  def change
+    create_table :change_requests_users, id: false do |t|
+    	t.belongs_to :user, index: true
+    	t.belongs_to :change_request, index: true
+    end
+  end
+end
