c875efe5a47549fa077f3ff5165c73d120336aa5
Muhammad Idham Ramadhan <muhammad.idham@veritrans.co.id>
2015-08-28 15:45:55 +0700

    Add Notification

--- app/models/change_request.rb
+++ app/models/change_request.rb
@@ -1,12 +1,14 @@
 class ChangeRequest < ActiveRecord::Base
   include AASM
   belongs_to :user
+  acts_as_readable :on => :updated_at
   has_and_belongs_to_many :collaborators, :class_name =>'User'
   has_many :testers, dependent: :destroy
   has_many :implementers, dependent: :destroy
   has_many :change_request_statuses, dependent: :destroy
   has_many :approvers, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :notifications
   belongs_to :cab
   scope :cab_free, -> {where(cab_id: nil)}
   acts_as_taggable
