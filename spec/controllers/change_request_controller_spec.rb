d5d56cd63091d20edd7f98462e28d4756b549790
Adam Pahlevi <adam.pahlevi@gmail.com>
2015-10-28 12:05:44 +0700

    update spec to expect approval date when it is approved

--- spec/controllers/change_request_controller_spec.rb
+++ spec/controllers/change_request_controller_spec.rb
@@ -145,6 +145,8 @@ describe ChangeRequestsController do
         put :approve, id: @cr
         @approval.reload
         expect(@approval.approve).to eq true
+        expect(@approval.approval_date).to_no be_nil
+        expect(@approval.approval_date.class).to eq(ActiveSupport::TimeWithZone)
       end 
     end
      describe 'PUT #reject' do
