1874bd92143f412e5eee58fc6ca42ce97f230793
Adam Pahlevi <adam.pahlevi@gmail.com>
2015-10-28 10:07:00 +0700

    performance checking, only check when meet_date is dirty... not only when it is first created

--- app/models/cab.rb
+++ app/models/cab.rb
@@ -2,9 +2,11 @@ class Cab < ActiveRecord::Base
   extend SimpleCalendar
   has_many :change_requests, dependent: :nullify
   validates :meet_date, presence:true, uniqueness:true
-  validate :validate_meet_date, :on => :create
+  
+  # validate meeting date all the time it is modified!
+  validate :validate_meet_date, if: :meet_date_changed?
   PARTICIPANTS = %w(cr@***REMOVED*** it_operation@***REMOVED*** stig@***REMOVED***)
   def validate_meet_date
-    errors.add("Meet date", "is invalid.") unless meet_date!=nil && meet_date > Time.now
+    errors.add("Meet date", "is invalid.") if meet_date.nil? || meet_date < Time.now
   end
 end
