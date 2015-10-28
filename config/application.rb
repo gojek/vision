8a9290b08de72175e03538a2e5302f1728046982
Adam Pahlevi <adam.pahlevi@gmail.com>
2015-10-28 14:09:46 +0700

    assets font without star

--- config/application.rb
+++ config/application.rb
@@ -31,9 +31,7 @@ module Vision
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.precompile << "fonts/*"
-    config.assets.precompile += %w( 
-    *.svg *.eot *.woff *.ttf *.png *.jpg *.jpeg *.gif)
+    config.assets.precompile += %w(.svg .eot .woff .ttf .png .jpg .jpeg .gif)
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
