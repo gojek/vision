31993078086819f01e0ec8023140c6a8575087d7
Dwiyan Rahmanianto <mohamad.dwiyan@ui.ac.id>
2016-06-24 11:03:55 +0700

    Refactoring rspec

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe CommentsController do
+	let(:change_request) {FactoryGirl.create(:change_request)}
 	before :each do
 		@request.env['devise.mapping'] = Devise.mappings[:user]
 		release_manager = FactoryGirl.create(:release_manager)
@@ -8,22 +9,16 @@ describe CommentsController do
 	end
 	describe 'POST #create' do
 		context "with valid attributes" do
-			before :each do
-				@cr = FactoryGirl.create(:change_request)
-			end
 			it 'saves the new comment in the databse' do
 				expect{
-					post :create, change_request_id: @cr.id, comment: {body: 'comment'}
+					post :create, change_request_id: change_request.id, comment: {body: 'comment'}
 				}.to change(Comment, :count).by(1)
 			end
 		end
 		context 'with invalid attributes' do
-			before :each do
-				@cr = FactoryGirl.create(:change_request)
-			end
 			it 'doesnt save the new comment in the databse' do
 				expect{
-					post :create, change_request_id: @cr.id, comment: {body: ''}
+					post :create, change_request_id: change_request.id, comment: {body: ''}
 				}.to_not change(Comment, :count)
 			end
 		end
