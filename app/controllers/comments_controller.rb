e37f865e7ae6d8b37265f69fb36fb1e671f0f34e
Dwiyan Rahmanianto <mohamad.dwiyan@ui.ac.id>
2016-06-27 12:04:08 +0700

    Adding slack notify functionality each time someone metioned in comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,8 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
   require 'mentioner.rb'
-  #before_action :set_comment, only: [:show, :edit, :update, :destroy]
+  require 'slack_notif.rb'
 
-  # GET /comments
-  # GET /comments.json
-
-
-  # POST /comments
-  # POST /comments.json
   def create
     @cr = ChangeRequest.find(params[:change_request_id])
     @comment = @cr.comments.new(comment_params)
@@ -20,9 +14,8 @@ class CommentsController < ApplicationController
 
         # Placeholder content for slack notification
         mentionees =  Mentioner.process_mentions(@comment)
-        mentionees.each do |mtn|
-          puts mtn.name
-        end
+        link = url_for @cr
+        SlackNotif.notif_comment_mention @comment, mentionees, link
 
         format.html { redirect_to @cr}
         format.json { render :show, status: :created, location: @comment }
@@ -33,56 +26,7 @@ class CommentsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /comments/1
-  # PATCH/PUT /comments/1.json
-=begin
- def index
-    @comments = Comment.all
-  end
-
-  # GET /comments/1
-  # GET /comments/1.json
-  def show
-  end
-
-  # GET /comments/new
-  def new
-    @comment = Comment.new
-  end
-
-  # GET /comments/1/edit
-  def edit
-  end
-  def update
-    respond_to do |format|
-      if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @comment }
-      else
-        format.html { render :edit }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /comments/1
-  # DELETE /comments/1.json
-  def destroy
-    @cr = ChangeRequest.find(params[:change_request_id])
-    @comment = @cr.comments.find(params[:id])
-    @comment.destroy
-    respond_to do |format|
-      format.html { redirect_to @cr, notice: 'Comment was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-=end
   private
-    # Use callbacks to share common setup or constraints between actions.
-    #def set_comment
-      #@comment = Comment.find(params[:id])
-    #end
-
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
       params.require(:comment).permit(:body, :change_request_id, :user_id)
