b30ff7b5da9746ebbe643e5e547ecbbbdf0abfed
Dwiyan Rahmanianto <mohamad.dwiyan@ui.ac.id>
2016-06-29 16:32:59 +0700

    Adding unit test when sending slack notif to mentioned in comments

--- spec/lib/slack_notif_spec.rb
+++ spec/lib/slack_notif_spec.rb
@@ -31,5 +31,26 @@ describe SlackNotif do
     end
   end
 
-  
+  describe "Sending notification to mentionees about comment that they are mentioned" do
+    let(:comment) {FactoryGirl.create(:comment, user: user, change_request: change_request)}
+
+    context "When all user's slack usernames known" do
+      let(:other_user) {FactoryGirl.create(:approver)}
+
+      it "Send message to all appropriate mentionee" do
+        user.update(slack_username: "dwiyan")
+        other_user.update(slack_username: "kevin")
+
+        expect(slack_notifier).to receive(:chat_postMessage).twice
+        SlackNotif.notif_comment_mention(comment, [user, other_user], "http://example.com")
+      end
+    end
+
+    context "When user's slack username unknown" do
+      it "does not send the message" do
+        expect(slack_notifier).not_to receive(:chat_postMessage)
+        SlackNotif.notif_comment_mention(comment, [user], "http://example.com")
+      end
+    end
+  end
 end
