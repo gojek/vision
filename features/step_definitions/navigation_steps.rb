d0f44b8ecf5d43aa6825639b01b0cfaa40e76398
Dwiyan Rahmanianto <mohamad.dwiyan@ui.ac.id>
2016-06-30 13:12:01 +0700

    Adding login step for all tests in cucumber

---
+++ features/step_definitions/navigation_steps.rb
@@ -0,0 +1,15 @@
+Given /^I visit page "(.*?)"$/ do |page|
+  visit page
+end
+
+When /^I press button "(.*?)"$/ do |button|
+  click_button(button)
+end
+
+When /^I input "(.*?)" in field "(.*?)"$/ do |value, field_name|
+
+end
+
+Then /^I am in homepage$/ do
+  pending
+end
