2deadb2da3c28cef4a5f10fd0af49f9127dc70e8
Kevin Aditya Prabowo <kevinster73@gmail.com>
2016-07-04 16:20:32 +0700

    Added cucumber testing for clear notification

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -9,3 +9,8 @@ end
 Given /^I am logged in as "(.*?)"$/ do |name|
   login_as User.find_by(name: name)
 end
+
+Given /^I am logged in as approver$/ do
+  @approver = FactoryGirl.create(:approver)
+  login_as @approver
+end 
