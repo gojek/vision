dbd38a3117739b5803e3e9987cf6e7aca0ea5161
Kevin Aditya Prabowo <kevinster73@gmail.com>
2016-07-04 14:29:02 +0700

    Resolve cherrypick commit from test cucumber for temporary testing

---
+++ features/login.feature
@@ -0,0 +1,9 @@
+Feature: Login
+As an example to login from cucumber
+
+Scenario: User log in
+Given I am logged in
+
+Scenario: User log in
+Given a user named "Dwiyan"
+And I am logged in as "Dwiyan"
---
+++ features/support/auth_helper.rb
@@ -0,0 +1,17 @@
+module AuthHelper
+  def login_as(user)
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.add_mock(:google_oauth2, {
+      uid: user.uid,
+      credentials: {
+        token: "token",
+        refresh_token: "refresh_token",
+        expires_at: Time.now + 1.hour
+      }
+    })
+    visit("/users/auth/google_oauth2")
+    OmniAuth.config.test_mode = false
+  end
+end
+
+World(AuthHelper)
