6ecc1deb929c05041d5436592599978f74451745
Kevin Aditya Prabowo <kevinster73@gmail.com>
2016-07-05 11:40:41 +0700

    Added scope for approver, and testing for User.approver in user model

--- app/controllers/change_requests_controller.rb
+++ app/controllers/change_requests_controller.rb
@@ -50,7 +50,7 @@ class ChangeRequestsController < ApplicationController
     @current_approvers = []
 
     @users = User.all.collect{|u| [u.name, u.id]}
-    @approvers = User.where(role: 'approver').collect{|u| [u.name, u.id]}
+    @approvers = User.approvers.collect{|u| [u.name, u.id]}
     @current_implementers = []
     @current_testers = []
   end
@@ -62,7 +62,7 @@ class ChangeRequestsController < ApplicationController
     @current_implementers = @change_request.implementers.collect{|u| u.id}
     @current_testers = @change_request.testers.collect{|u| u.id}
     @users = User.all.collect{|u| [u.name, u.id]}
-    @approvers = User.where(role: 'approver').collect{|u| [u.name, u.id]}
+    @approvers = User.approvers.collect{|u| [u.name, u.id]}
     @current_approvers = @change_request.approvals.collect(&:user_id)
   end
 
@@ -128,7 +128,7 @@ class ChangeRequestsController < ApplicationController
         @tags = ActsAsTaggableOn::Tag.all.collect(&:name)
         @current_tags = []
         @users = User.all.collect{|u| [u.name, u.id]}
-        @approvers = User.where(role: 'approver').collect{|u| [u.name, u.id]}
+        @approvers = User.approvers.collect{|u| [u.name, u.id]}
         @current_collaborators = params[:collaborators_list]? params[:collaborators_list] : []
         @current_implementers = params[:implementers_list]? params[:implementers_list] : []
         @current_testers = params[:testers_list]? params[:testers_list] : []
@@ -190,7 +190,7 @@ class ChangeRequestsController < ApplicationController
         @current_implementers = @change_request.implementers.collect{|u| u.id}
         @current_testers = @change_request.testers.collect{|u| u.id}
         @users = User.all.collect{|u| [u.name, u.id]}
-        @approvers = User.where(role: 'approver').collect{|u| [u.name, u.id]}
+        @approvers = User.approvers.collect{|u| [u.name, u.id]}
         format.html { render :edit }
         format.json { render json: @change_request.errors, status: :unprocessable_entity }
       end
