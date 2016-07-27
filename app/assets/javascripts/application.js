9ca9608807ddb30e2a5a9ba2e19733fce9905c49
Rafeequl <rafeequl@veritrans.co.id>
2016-07-27 16:54:21 +0700

    force to use jquery2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,7 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//= require jquery
+//= require jquery2
 //= require jquery.turbolinks
 //= require jquery_ujs
 //= require jquery-ui
