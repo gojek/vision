4e4b39bead02e1f311fcdaf1509b97efc1f3addb
Dwiyan Rahmanianto <mohamad.dwiyan@ui.ac.id>
2016-08-03 15:22:26 +0700

    create associated user list for a change request

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,8 @@ class User < ActiveRecord::Base
   validates :email, presence: true
   has_many :IncidentReports
   has_many :ChangeRequests
-  has_and_belongs_to_many :collaborate_change_requests, join_table: :collaborators, :class_name => 'ChangeRequest'
+  has_and_belongs_to_many :associated_change_requests, join_table: :change_requests_associated_users, class_name: 'ChangeRequest'
+  has_and_belongs_to_many :collaborate_change_requests, join_table: :collaborators, class_name: 'ChangeRequest'
   has_and_belongs_to_many :implement_change_requests, join_table: :implementers, class_name: :ChangeRequest
   has_and_belongs_to_many :test_change_requests, join_table: :testers, class_name: :ChangeRequest
   has_many :Comments
