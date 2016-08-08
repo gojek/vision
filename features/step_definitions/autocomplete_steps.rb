91c1837813a3c37de934e474ce1a9bfdc8c70a52
Kevin Aditya Prabowo <kevinster73@gmail.com>
2016-08-08 14:53:36 +0700

    Fixed cucumber steps and moved array to controller

--- features/step_definitions/autocomplete_steps.rb
+++ features/step_definitions/autocomplete_steps.rb
@@ -17,10 +17,6 @@ When(/^I input @ in the comment field$/) do
   fill_in("comment", with: "@")
 end
 
-When /^wait$/ do
-  sleep 2
-end
-
 Then(/^it should show suggestions that shows all users name$/) do
   page.should have_selector(:xpath, "//div[@class='atwho-view' and @style]")
 end
