656fca6dbafb75b1fbe640770371984ff902e444
Dwiyan Rahmanianto <mohamad.dwiyan@ui.ac.id>
2016-08-15 17:59:02 +0700

    refactoring and adding new function to slack notif

--- lib/slack_notif.rb
+++ lib/slack_notif.rb
@@ -1,54 +1,61 @@
+require 'slack_attachment_builder.rb'
+require 'mentioner.rb'
+
 class SlackNotif
-  def self.client
-    @client ||= Slack::Web::Client.new
+  include Rails.application.routes.url_helpers
+
+  def initialize
+    @client = Slack::Web::Client.new
+    @attachment_builder = SlackAttachmentBuilder.new
   end
 
-  def self.notif_new_request (change_request,link)
-    approvers = change_request.approvals.collect{|app| app.user}
-    approvers.each do |approver|
-      next if approver.slack_username.blank?
-      message = "New <#{link}|change request> needs your approvals"
-      attachment = {
-        fallback: message,
-        color: "#439FE0",
-        title: change_request.change_summary,
-        title_link: link,
-        fields: [
-          {
-            title: "Business Justification",
-            value: change_request.business_justification,
-            short: false
-          },{
-            title: "Priority",
-            value: change_request.priority,
-            short: true
-          },{
-            title: "Scope",
-            value: change_request.scope,
-            short: true
-          },
-        ],
-        footer: "VT-Vision",
-        ts: change_request.created_at.to_datetime.to_f.round
-      }
-      self.client.chat_postMessage(channel: "@#{approver.slack_username}", text: message, attachments: [attachment])
+  def notify_new_cr(change_request)
+    attachment = @attachment_builder.generate_change_request_attachment(change_request)
+    link = change_request_url(change_request)
+    approvers = change_request.approvals.collect{|approval| approval.user}
+    approver_message = "New <#{link}|change request> needs your approvals"
+    message_users(approvers, approver_message, attachment)
+    associated_users = change_request.associated_users
+    general_message = "New <#{link}|change request> has been created"
+    message_users(associated_users - approvers, general_message, attachment)
+    message_channel('cab', general_message, attachment)
+  end
+
+  def notify_update_cr(change_request)
+    attachment = @attachment_builder.generate_change_request_attachment(change_request)
+    link = change_request_url(change_request)
+    approvers = change_request.approvals.collect{|approval| approval.user}
+    approver_message = "Modified <#{link}|change request> needs your approvals"
+    message_users(approvers, approver_message, attachment)
+    associated_users = change_request.associated_users
+    general_message = "<#{link}|Change request> has been modified"
+    message_users(associated_users - approvers, general_message, attachment)
+    message_channel('cab', general_message, attachment)
+  end
+
+  def notify_new_comment(comment)
+    attachment = @attachment_builder.generate_comment_attachment(comment)
+    link = change_request_url(comment.change_request)
+    mentionees =  Mentioner.process_mentions(comment)
+    if !mentionees.empty?
+      mentioned_message = "You are mentioned in #{comment.user.name} comment's on a <#{link}|change request>"
+      message_users(mentionees, mentioned_message, attachment)
+    end
+    associated_users = comment.change_request.associated_users
+    general_message = "A new comment from #{comment.user.name} on a <#{link}|change request>"
+    message_users(associated_users - mentionees, general_message, attachment)
+  end
+
+  private
+  def message_users(users, message, attachment)
+    users.each do |user|
+      next if user.slack_username.blank?
+      @client.chat_postMessage(channel: "@#{user.slack_username}", text: message, attachments: [attachment])
     end
   end
 
-  def self.notif_comment_mention(comment, mentionees, link)
-    mentionees.each do |mentionee|
-      next if mentionee.slack_username.blank?
-      attachment = {
-        fallback: comment.body,
-        text: comment.body,
-        color: "#439FE0",
-        title: comment.change_request.change_summary,
-        title_link: link,
-        footer: "VT-Vision",
-        ts: comment.created_at.to_datetime.to_f.round
-      }
-      self.client.chat_postMessage(channel: "@#{mentionee.slack_username}",
-        text: "You are mentioned in #{comment.user.name} comment's on <#{link}|change request>", attachments: [attachment])
-    end
+  def message_channel(channel, message, attachment)
+    @client.chat_postMessage(channel: "##{channel}", text: message, attachments: [attachment])
   end
+
 end
