0bcfa51d698bb581697b29dd1e2365f5388c29ea
Dwiyan Rahmanianto <mohamad.dwiyan@ui.ac.id>
2016-08-15 17:57:55 +0700

    Refactoring mentioner now using more descriptive naming

--- spec/lib/mentioner_spec.rb
+++ spec/lib/mentioner_spec.rb
@@ -4,42 +4,35 @@ require 'mentioner'
 describe Mentioner do
   let(:user) {FactoryGirl.create(:user)}
   let(:change_request) {FactoryGirl.create(:change_request, user: user)}
-  let(:comment) {FactoryGirl.create(:comment, user: user, change_request: change_request)}
   let(:comment_with_mention) {FactoryGirl.create(:comment_with_mention, user: user, change_request: change_request)}
-  describe 'Mentioner.extract_mentioner_content' do
-    it 'should return the body of the comment' do
-      body = Mentioner.extract_mentioner_content(comment)
-      expect(body).to eq(comment.body)
-    end
-  end
 
   describe 'Mentioner.extract_handles_from_mentioner' do
     it 'should return array of usernames (email headers) from the comment body' do
-      handles = Mentioner.extract_handles_from_mentioner(comment_with_mention)
+      usernames = Mentioner.extract_username(comment_with_mention)
       content = comment_with_mention.body.gsub('@','').split(" ")
-      expect(handles).to match_array(content)
+      expect(usernames).to match_array(content)
     end
   end
 
-  describe 'Mentioner.find_mentionees_by_handles' do
+  describe 'Mentioner.find_mentionees_from_username' do
     it 'should return an array of users with the email headers stated in handles' do
-      handles = Mentioner.extract_handles_from_mentioner(comment_with_mention)
+      usernames = Mentioner.extract_username(comment_with_mention)
       users = []
-      handles.each do |handle|
-        users << FactoryGirl.create(:user, email: handle + "@veritrans.co.id")
+      usernames.each do |username|
+        users << FactoryGirl.create(:user, email: username + "@veritrans.co.id")
       end
-      expect(Mentioner.find_mentionees_by_handles(handles)).to match_array(users)
+      expect(Mentioner.find_mentionees_from_username(usernames)).to match_array(users)
     end
   end
 
   describe 'Mentioner.process_mentions' do
-    it 'should call extract_handles_from_mentioner method' do
-      expect(Mentioner).to receive(:extract_handles_from_mentioner).with(comment_with_mention)
+    it 'should call extract_username method' do
+      expect(Mentioner).to receive(:extract_username).with(comment_with_mention).and_call_original
       Mentioner.process_mentions(comment_with_mention)
     end
 
-    it 'should call find_mentionees_by_handles' do
-      expect(Mentioner).to receive(:find_mentionees_by_handles)
+    it 'should call find_mentionees_from_username' do
+      expect(Mentioner).to receive(:find_mentionees_from_username)
       Mentioner.process_mentions(comment_with_mention)
     end
   end
@@ -47,6 +40,12 @@ describe Mentioner do
   describe 'Mentioner.mention_prefix' do
     it 'should return @ when called' do
       expect(Mentioner.mention_prefix).to eq "@"
+    end
+  end
+
+  describe 'Mentioner.username_extract_regex' do
+    it 'should return return regex' do
+      expect(Mentioner.username_extract_regex).to be_a Regexp
     end
   end
 end
