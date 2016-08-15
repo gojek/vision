a46c415c18a2866259bbe9ecdb4a0d055334e00d
Dwiyan Rahmanianto <mohamad.dwiyan@ui.ac.id>
2016-08-15 17:54:45 +0700

    configuration for default routes host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  #Setting for host domain for route
+  Rails.application.routes.default_url_options = { host: 'localhost', port: 3003 }
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
