90a6ee6f6ce75d4237fa6700eaabb4dae8a34030
Kevin Aditya Prabowo <kevinster73@gmail.com>
2016-08-15 13:12:04 +0700

    Remove more unused steps

--- features/step_definitions/autocomplete_steps.rb
+++ features/step_definitions/autocomplete_steps.rb
@@ -1,22 +1,3 @@
-Given(/^someone has made a change request$/) do
-
-  with_versioning do
-    expect(PaperTrail.should be_enabled).to eq true
-    @user = FactoryGirl.create(:user)
-    @cr = FactoryGirl.create(:change_request, user: @user)
-    @version = @cr.versions.first
-    @version.update(whodunnit: @user.id)
-  end
-end
-
-Given(/^I am in that change request's show page$/) do
-  visit("/change_requests/#{@cr.id}")
-end
-
-When(/^I input @ in the comment field$/) do
-  fill_in("comment", with: "@")
-end
-
 Then(/^it should show suggestions that shows all users name$/) do
   page.should have_selector(:xpath, "//div[@class='atwho-view' and @style]")
 end
--- features/step_definitions/clear_notification_steps.rb
+++ features/step_definitions/clear_notification_steps.rb
@@ -1,19 +1,3 @@
-Given(/^I have any change request \/ incident report notifications$/) do
-  @user = FactoryGirl.create(:user)
-  @cr = FactoryGirl.create(:change_request, user: @user)
-  @ir = FactoryGirl.create(:incident_report, user: @user)
-  @notifcr = FactoryGirl.create(:notification, user: @approver, change_request: @cr)
-  @notifir = FactoryGirl.create(:notification, user: @approver, incident_report: @ir)
-  visit root_path
-  page.should have_content("Dashboard")
-  page.should have_selector(:xpath, "//span[@id='notif-cr']")
-  page.should have_selector(:xpath, "//span[@id='notif-ir']")
-end
-
-When(/^I click the clear notification button$/) do
-  page.find_link('clear-notification').click
-end
-
 Then(/^all notifications should be removed$/) do
   page.should_not have_selector(:xpath, "//span[@id='notif-cr']")
   page.should_not have_selector(:xpath, "//span[@id='notif-ir']")
