70b5824e123f1650b4b9f82a40860b68a6a79185
Adam Pahlevi <adam.pahlevi@gmail.com>
2016-12-13 15:01:54 +0700

    fix deprecation errors

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -109,7 +109,7 @@ RSpec.configure do |config|
   #     --seed 1234
   config.order = "random"
 
-  config.include Devise::TestHelpers, :type => :controller
+  config.include Devise::Test::ControllerHelpers, :type => :controller
 
 end
 Shoulda::Matchers.configure do |config|
