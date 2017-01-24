eea21d7a3a6b9caf33d4c87a1a4f9bacfe517ac1
rseptiandry <raden.septiandry@midtrans.com>
2017-01-24 10:04:35 +0700

    Correcting indentations

--- spec/features/login_session_spec.rb
+++ spec/features/login_session_spec.rb
@@ -7,15 +7,15 @@ RSpec.describe "Login session timeout testing", :type=>:feature do
     login_as(user)
   	login_time = Time.current
     #forward time to 14 days later  
-	Timecop.freeze(Time.current + 13.days) do
-      #login_time is last time user login
-	  expect(user.timedout?(login_time)).to eq(false)
-      visit edit_change_request_path(id: change_request.id)		
-  	  expect(page).to have_content("Change Request")
-	end
-  	  #forward time to 30 days later
-      Timecop.freeze(Time.current + 15.days) do
-	  expect(user.timedout?(login_time)).to eq(true)
-	end
+  	Timecop.freeze(Time.current + 13.days) do
+        #login_time is last time user login
+  	  expect(user.timedout?(login_time)).to eq(false)
+        visit edit_change_request_path(id: change_request.id)		
+    	  expect(page).to have_content("Change Request")
+  	end
+    #forward time to 30 days later
+    Timecop.freeze(Time.current + 15.days) do
+  	  expect(user.timedout?(login_time)).to eq(true)
+  	end
   end
 end
