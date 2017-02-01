35665e6de91aa165d764dda0f80faeb78af6a320
Aditya Rama <adityarama1210@github.com>
2017-02-01 17:58:51 +0700

    2 spaces

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -3,53 +3,53 @@ require 'slack_notif'
 require 'mentioner.rb'
 
 describe CommentsController do
-	let(:change_request) {FactoryGirl.create(:change_request)}
-	let(:cr_comment) {FactoryGirl.build(:comment, body: 'comment')}
-	before :each do
-		@request.env['devise.mapping'] = Devise.mappings[:user]
-		release_manager = FactoryGirl.create(:release_manager)
-		sign_in release_manager
-	end
-	describe 'POST #create' do
-		context "with valid attributes" do
-			it 'saves the new comment in the databse' do
-				expect{
-					post :create, change_request_id: change_request.id, comment: {body: 'comment'}
-				}.to change(Comment, :count).by(1)
-			end
+  let(:change_request) {FactoryGirl.create(:change_request)}
+  let(:cr_comment) {FactoryGirl.build(:comment, body: 'comment')}
+  before :each do
+    @request.env['devise.mapping'] = Devise.mappings[:user]
+    release_manager = FactoryGirl.create(:release_manager)
+    sign_in release_manager
+  end
+  describe 'POST #create' do
+    context "with valid attributes" do
+      it 'saves the new comment in the databse' do
+        expect{
+          post :create, change_request_id: change_request.id, comment: {body: 'comment'}
+        }.to change(Comment, :count).by(1)
+      end
 
-			it 'call slack notification library to notify to the mentionees that they have been mentioned' do
-				comment = FactoryGirl.build(:comment, body: 'comment @metionee')
-				expect_any_instance_of(SlackNotif).to receive(:notify_new_comment).with(an_instance_of(Comment))
-				post :create, change_request_id: change_request.id, comment: {body: comment.body}
-			end
-		end
-		context 'with invalid attributes' do
-			it 'doesnt save the new comment in the databse' do
-				expect{
-					post :create, change_request_id: change_request.id, comment: {body: ''}
-				}.to_not change(Comment, :count)
-			end
-		end
+      it 'call slack notification library to notify to the mentionees that they have been mentioned' do
+        comment = FactoryGirl.build(:comment, body: 'comment @metionee')
+        expect_any_instance_of(SlackNotif).to receive(:notify_new_comment).with(an_instance_of(Comment))
+        post :create, change_request_id: change_request.id, comment: {body: comment.body}
+      end
+    end
+    context 'with invalid attributes' do
+      it 'doesnt save the new comment in the databse' do
+        expect{
+          post :create, change_request_id: change_request.id, comment: {body: ''}
+        }.to_not change(Comment, :count)
+      end
+    end
 
-	end
-	describe 'POST #hide_unhide' do
-		context 'hide' do
-			it 'change the comment into hide state' do
-				change_request.comments << cr_comment
-				post :hide_unhide, change_request_id: change_request.id, comment_id: cr_comment.id, type: 'hide'
-				expect(Comment.find(cr_comment.id).hide).to eq(true) 
-			end
-		end
-		context 'unhide' do
-			it 'change the comment into unhide' do
-				change_request.comments << cr_comment
-				cr_comment.hide = true
-				cr_comment.save
-				post :hide_unhide, change_request_id: change_request.id, comment_id: cr_comment.id, type: 'unhide'
-				expect(Comment.find(cr_comment.id).hide).to eq(false) 
-			end
-		end
-	end
+  end
+  describe 'POST #hide_unhide' do
+    context 'hide' do
+      it 'change the comment into hide state' do
+        change_request.comments << cr_comment
+        post :hide_unhide, change_request_id: change_request.id, comment_id: cr_comment.id, type: 'hide'
+        expect(Comment.find(cr_comment.id).hide).to eq(true) 
+      end
+    end
+    context 'unhide' do
+      it 'change the comment into unhide' do
+        change_request.comments << cr_comment
+        cr_comment.hide = true
+        cr_comment.save
+        post :hide_unhide, change_request_id: change_request.id, comment_id: cr_comment.id, type: 'unhide'
+        expect(Comment.find(cr_comment.id).hide).to eq(false) 
+      end
+    end
+  end
 
 end
