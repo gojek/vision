c87279ab6436d7f4bacdc75adadc539a1565b080
rseptiandry <raden.septiandry@midtrans.com>
2017-02-01 13:58:19 +0700

    Create button in view to download csv or send to email

--- app/mailers/change_request_mailer.rb
+++ app/mailers/change_request_mailer.rb
@@ -2,7 +2,7 @@ class ChangeRequestMailer < ApplicationMailer
 	default from: "raden.septiandry@midtrans.com"
 
   def send_csv(cr_csv, email)
-   attachments['my_file_name.csv'] = {mime_type: 'text/csv', content: cr_csv}
-    mail(to: email, subject: 'My subject', body: 'My body.')
+   	attachments['change_requests.csv'] = {mime_type: 'text/csv', content: cr_csv}
+    mail(to: email, subject: 'Change Requests CSV', body: 'Change requests csv.')
   end
 end
