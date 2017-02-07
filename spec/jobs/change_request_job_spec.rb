fa7eefadea16f18d2dd147f3ea6e9378f5bdaa06
rseptiandry <raden.septiandry@midtrans.com>
2017-02-07 18:34:49 +0700

    Add unit test for background job

---
+++ spec/jobs/change_request_job_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe ChangeRequestJob do
+  let(:job) {ChangeRequestJob.new }	
+  let(:user) {FactoryGirl.create(:user, )}
+  let(:crs) {ChangeRequest.all}
+
+  describe "#perform" do
+    it "delivers an email" do 
+      expect{  
+              job.perform(crs.ids, user.email)
+      }.to change{ ActionMailer::Base.deliveries.size }.by(1)
+    end 
+  end 
+end
