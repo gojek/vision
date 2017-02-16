8276d4d87409162e330d7228b25ff0ab73d9e7a4
rseptiandry <raden.septiandry@midtrans.com>
2017-02-16 11:31:17 +0700

    Fix indentation and grammar

--- app/jobs/change_request_job.rb
+++ app/jobs/change_request_job.rb
@@ -1,17 +1,17 @@
-# app/jobs/log_job.rb
+# app/jobs/change_request_job.rb
 
 class ChangeRequestJob include SuckerPunch::Job 
 
   def perform(cr_ids, email)
-  	csv_string = CSV.generate do |csv|
-  		csv << ChangeRequest.attribute_names
-  		ChangeRequest.where(id: cr_ids).where.not(aasm_state: 'draft').order("created_at DESC").each do |cr|
-    		csv << cr.attributes.values
-  		end
-		end
+    csv_string = CSV.generate do |csv|
+      csv << ChangeRequest.attribute_names
+  	  ChangeRequest.where(id: cr_ids).where.not(aasm_state: 'draft').order("created_at DESC").each do |cr|
+        csv << cr.attributes.values
+  	  end
+	  end
 
     ActiveRecord::Base.connection_pool.with_connection do
-			ChangeRequestMailer.send_csv(csv_string, email).deliver
+		  ChangeRequestMailer.send_csv(csv_string, email).deliver
     end
   end
 end
