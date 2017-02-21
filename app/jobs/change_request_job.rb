1f6b4ac1fe8c038f06e630ca2748bf0db14fcf30
rseptiandry <raden.septiandry@midtrans.com>
2017-02-21 14:06:42 +0700

    Fix class naming indentation

--- app/jobs/change_request_job.rb
+++ app/jobs/change_request_job.rb
@@ -1,11 +1,12 @@
 # app/jobs/change_request_job.rb
 
-class ChangeRequestJob include SuckerPunch::Job 
+class ChangeRequestJob 
+  include SuckerPunch::Job 
 
   def perform(cr_ids, email)
     csv_string = CSV.generate do |csv|
       csv << ChangeRequest.attribute_names
-  	  ChangeRequest.where(id: cr_ids).where.not(aasm_state: 'draft').order("created_at DESC").each do |cr|
+      ChangeRequest.where(id: cr_ids).where.not(aasm_state: 'draft').order("created_at DESC").each do |cr|
         csv << cr.attributes.values
   	  end
 	  end
