57e104f07ca140d85164259d32e67d2dede857b9
Aditya Rama <adityarama1210@github.com>
2017-02-28 12:35:28 +0700

    adding spec for incident report job

---
+++ spec/jobs/incident_report_job_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+require 'sucker_punch/testing/inline'
+
+describe IncidentReportJob, :type => :job do
+  let(:user) {FactoryGirl.create(:user)}
+  let(:irs) {FactoryGirl.create_list(:incident_report,10, user: user)}
+
+  describe "#perform" do
+    it "delivers an email to a vaild sender with valid attributes" do
+      ActiveJob::Base.queue_adapter = :test
+      irs.map! {|ir| ir.id }
+      IncidentReportJob.perform_async(irs, user.email)
+      expect(ActionMailer::Base.deliveries.size).to eq 1
+      email = ActionMailer::Base.deliveries.first
+      expect(email.subject).to eq "Incident Report CSV"
+      expect(email.to[0]).to eq user.email
+    end 
+  end 
+end
