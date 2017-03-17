64bd44171aa9e4892e44859ba7794a6537eed879
rseptiandry <raden.septiandry@midtrans.com>
2017-03-17 16:44:46 +0700

    local change

--- app/models/change_request.rb
+++ app/models/change_request.rb
@@ -21,7 +21,11 @@ class ChangeRequest < ActiveRecord::Base
   STATUS = %w(submitted scheduled rollbacked cancelled rejected deployed closed)
 
   validates :change_summary, :priority,:change_requirement, :business_justification, :analysis, :solution, :impact, :scope, :design,
+<<<<<<< HEAD
             :backup, :testing_procedure, :testing_notes, :schedule_change_date, :planned_completion, :definition_of_success, :definition_of_failed,:grace_period_end, :grace_period_starts, presence: true
+=======
+            :backup, :testing_procedure, :testing_notes, :schedule_change_date, :planned_completion, :definition_of_success, :definition_of_failed, presence: true
+>>>>>>> e3544cb... remove grace start and grace period from mandatory field
   validates_inclusion_of :testing_environment_available, :in => [true, false]
   accepts_nested_attributes_for :implementers, reject_if: :all_blank, allow_destroy: true
   accepts_nested_attributes_for :testers, :allow_destroy => true
