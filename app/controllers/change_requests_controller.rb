bb6b90f8aae0dae2a20ceeca5b1695522604487f
rseptiandry <raden.septiandry@midtrans.com>
2017-03-24 14:01:36 +0700

    remove comment

--- app/controllers/change_requests_controller.rb
+++ app/controllers/change_requests_controller.rb
@@ -129,7 +129,6 @@ class ChangeRequestsController < ApplicationController
         @change_request.save(:validate=> false)
         flash[:notice] = 'Change request was created as a draft.'
         flash[:invalid] = @change_request.errors.full_messages
-        puts(@error)
         @status = @change_request.change_request_statuses.new(:status => 'draft')
         @status.save
       else
