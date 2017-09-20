44b87d1488eba6d103309d64c79c98bf077f6a26
Muhammad.Maulana <muhammad.maulana@midtrans.com>
2017-09-20 10:16:41 +0700

    Change access_type to online

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_API_KEY'], ENV['GOOGLE_API_SECRET'], {
   	:prompt => 'consent',
 
-  	:access_type => 'offline',
+  	:access_type => 'online',
   	:scope => 'profile,email,calendar,https://www.google.com/m8/feeds/'
   }
 end
