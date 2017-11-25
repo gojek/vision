26ddb7e784887b8c65333273ddcdc03e56687710
V Sreekanth <sreekanth@veritrans.co.id>
2017-11-25 18:48:31 +0700

    Add downtime fields and moving some of the cr listing fields

---
+++ db/migrate/20171123200703_add_downtime_to_cr.rb
@@ -0,0 +1,6 @@
+oclass AddDowntimeToCr < ActiveRecord::Migration
+  def change
+    add_column :change_requests, :downtime_expected, :boolean
+    add_column :change_requests, :expected_downtime_in_minutes, :integer
+  end
+end
