2e98d1ffb9565afc7fb04976d2fba9015e9e6514
V Sreekanth <sreekanth@veritrans.co.id>
2017-11-25 19:48:37 +0700

    Rolling back the default

--- db/migrate/20171123200703_add_downtime_to_cr.rb
+++ db/migrate/20171123200703_add_downtime_to_cr.rb
@@ -1,6 +1,6 @@
-oclass AddDowntimeToCr < ActiveRecord::Migration
+class AddDowntimeToCr < ActiveRecord::Migration
   def change
-    add_column :change_requests, :downtime_expected, :boolean, default: false
-    add_column :change_requests, :expected_downtime_in_minutes, :integer, null: true
+    add_column :change_requests, :downtime_expected, :boolean,
+    add_column :change_requests, :expected_downtime_in_minutes, :integer
   end
 end
