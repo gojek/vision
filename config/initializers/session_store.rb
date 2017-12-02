db771374acb342a0230ade1579df59404eaf1fa4
V Sreekanth <sreekanth@veritrans.co.id>
2017-12-02 17:49:34 +0700

    fixup! Messing aroudn with the login prompts

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
-f# Be sure to restart your server when you modify this file.
+# Be sure to restart your server when you modify this file.
 
 Rails.application.config.session_store :cookie_store, key: '_vision_session'
