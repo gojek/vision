06cf874e5c48d3f1581e0ac6d419ee5aee0940de
V Sreekanth <sreekanth@veritrans.co.id>
2017-12-02 19:13:35 +0700

    Removing simple logs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,10 +15,9 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-#    session[:first_time] = true
+    session[:first_time] = true
     return_url = stored_location_for(resource) || change_requests_path
     logger.info "Returning User to.......... #{return_url}"
-    logger.info "-----------------"
     return_url
   end
 
