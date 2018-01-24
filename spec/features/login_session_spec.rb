f9f4ade51be1e0414c9fec08df2b5b2dbb9ae933
Pavel Evstigneev <pavel.evst@gmail.com>
2018-01-24 19:16:29 +0800

    Found a cause of short sessions

--- spec/features/login_session_spec.rb
+++ spec/features/login_session_spec.rb
@@ -1,20 +1,21 @@
 require "spec_helper"
 
-RSpec.describe "Login session timeout testing", :type=>:feature do
+RSpec.describe "Login session timeout testing", :type => :feature do
   it "User login session should be in two weeks" do
     user = FactoryGirl.create(:user)
-    change_request = FactoryGirl.create(:change_request, user: user)
     login_as(user)
     login_time = Time.current
-    #forward time to 14 days later  
+
+    #forward time to 14 days later
     Timecop.freeze(Time.current + 13.days) do
       #login_time is last time user login
       expect(user.timedout?(login_time)).to eq(false)
-      visit edit_change_request_path(id: change_request.id)   
+      visit new_change_request_path
       expect(page).to have_content("Change Request")
     end
+
     #forward time to 30 days later
-    Timecop.freeze(Time.current + 15.days) do
+    Timecop.freeze(Time.current + 30.days) do
       expect(user.timedout?(login_time)).to eq(true)
     end
   end
