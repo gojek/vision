ae3c3ced8e9845a67d7116eeeb1a7aa99248e1cd
Pavel Evstigneev <pavel.evst@gmail.com>
2018-01-24 19:16:29 +0800

    Found a cause of short sessions

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,8 @@
 # 'config/initializers/omniauth.rb'
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_API_KEY'], ENV['GOOGLE_API_SECRET'], {
-        :access_type => 'offline',
-        :scope => 'profile,email,calendar,https://www.google.com/m8/feeds/'
+    access_type: 'offline',
+    hd: "***REMOVED***",
+    scope: 'profile,email,calendar,https://www.google.com/m8/feeds/'
   }
 end
