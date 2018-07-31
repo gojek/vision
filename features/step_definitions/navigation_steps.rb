4f782548b1efffba8b1cc98c6758b5145a84baf2
adirizka7 <adiunity@gmail.com>
2018-07-31 15:10:25 +0700

    add specs for quick filter and access_request_bug

--- features/step_definitions/navigation_steps.rb
+++ features/step_definitions/navigation_steps.rb
@@ -6,6 +6,12 @@ When /^I visit page "(.*?)"$/ do |page|
   visit page
 end
 
+When /^I update access request with employee name "([^"]*)"$/ do |employee_name|
+  ar = AccessRequest.where(employee_name: employee_name).first
+  visit "/access_requests/#{ar.id}/edit"
+  click_button("Update Access request")
+end
+
 When /^I press button "(.*?)"$/ do |button|
   click_button(button)
 end
@@ -34,7 +40,13 @@ Then(/^I should be redirected to a new change request page$/) do
   page.should have_content("Testing")
   page.should have_content("Implementation")
 end
+
 When /^I visit change request with change summary "(.*?)"$/ do |change_summary|
   cr = ChangeRequest.where(change_summary: change_summary).first
   visit "/change_requests/#{cr.id}"
+end
+
+When /^I visit access request with employee name "(.*?)"$/ do |employee_name|
+  ar = AccessRequest.where(employee_name: employee_name).first
+  visit "/access_requests/#{ar.id}"
 end
