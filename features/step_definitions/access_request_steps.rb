51e927eabd25d2fd6ab2e5f54dc6bfb5f3cb8bd9
adirizka7 <adiunity@gmail.com>
2018-08-23 13:03:31 +0700

    add cucumber test for ar comment

--- features/step_definitions/access_request_steps.rb
+++ features/step_definitions/access_request_steps.rb
@@ -1,3 +1,11 @@
+Given /^an access request with employee name "([^"]*)"$/ do |employee_name|
+  user = FactoryGirl.create(:user)
+  ar = FactoryGirl.create(:access_request, user: user, employee_name: employee_name)
+  version = ar.versions.first
+  version.whodunnit = user.id
+  version.save!
+end
+
 Given /^an access request with employee name "([^"]*)" with "([^"]*)" as business justification$/ do |employee_name, business_justification|
   user = FactoryGirl.create(:user)
   ar = FactoryGirl.create(:access_request, user: user, employee_name: employee_name, business_justification: business_justification)
