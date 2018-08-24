092df21bf8d70e4e3e9ca1b98998b5119d3a29cc
adirizka7 <adiunity@gmail.com>
2018-08-24 17:19:46 +0700

    fix error on refresh_token

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,9 @@
 # 'config/initializers/omniauth.rb'
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_API_KEY'], ENV['GOOGLE_API_SECRET'], {
-    access_type: 'offline',
-    hd: '*',
-    scope: 'profile,email,calendar,https://www.google.com/m8/feeds/'
+  	:prompt => 'consent',
+
+  	:access_type => 'offline',
+  	:scope => 'profile,email,calendar,https://www.google.com/m8/feeds/'
   }
 end
