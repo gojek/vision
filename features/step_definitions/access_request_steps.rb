66688fc9dd5b7f173418dc363c67c0359f79eaa6
adirizka7 <adiunity@gmail.com>
2018-09-03 13:03:26 +0700

    breakdown test for approval comment and approval time

--- features/step_definitions/access_request_steps.rb
+++ features/step_definitions/access_request_steps.rb
@@ -15,26 +15,14 @@ Given(/^an access request with employee name "([^"]*)" that needs my approval$/)
   approval = AccessRequestApproval.create(user: @current_user, access_request: ar, approved: nil, notes: nil)
 end
 
-Given(/^an access request with employee name "([^"]*)" that has been approved with comment "([^"]*)" and "([^"]*)"$/) do |employee_name, comment, comment2|
-  user = FactoryGirl.create(:user)
+Given(/^an access request approved comment "([^"]*)" with employee name "([^"]*)"$/) do |comment, employee_name|
+  ar = AccessRequest.where(:employee_name => employee_name).first
   approver = FactoryGirl.create(:approver_ar)
-  approver2 = FactoryGirl.create(:approver_ar)
-  ar = FactoryGirl.create(:access_request, user: user, employee_name: employee_name, aasm_state: "submitted")
-  version = ar.versions.first
-  version.whodunnit = user.id
-  version.save!
   approval = AccessRequestApproval.create(user: approver, access_request: ar, approved: true, notes: comment)
-  approval2 = AccessRequestApproval.create(user: approver2, access_request: ar, approved: true, notes: comment2)
 end
 
-Given(/^an access request with employee name "([^"]*)" that has been approved on "([^"]*)" and "([^"]*)"$/) do |employee_name, date1, date2|
-  user = FactoryGirl.create(:user)
+Given(/^an access request approval on "([^"]*)" with employee name "([^"]*)"$/) do |time_approved, employee_name|
+  ar = AccessRequest.where(:employee_name => employee_name).first
   approver = FactoryGirl.create(:approver_ar)
-  approver2 = FactoryGirl.create(:approver_ar)
-  ar = FactoryGirl.create(:access_request, user: user, employee_name: employee_name, aasm_state: "submitted")
-  version = ar.versions.first
-  version.whodunnit = user.id
-  version.save!
-  approval = AccessRequestApproval.create(user: approver, access_request: ar, approved: true, notes: "Cool", updated_at: date1)
-  approval2 = AccessRequestApproval.create(user: approver2, access_request: ar, approved: true, notes: "Mantap", updated_at: date2)
+  approval = AccessRequestApproval.create(user: approver, access_request: ar, approved: true, notes: "Ok", updated_at: time_approved)
 end
