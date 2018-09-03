163544842dc335688070563493b93ca929668dae
adirizka7 <adiunity@gmail.com>
2018-09-03 15:44:06 +0700

    fix typos ir

--- config/routes.rb
+++ config/routes.rb
@@ -81,12 +81,18 @@ Rails.application.routes.draw do
   resources :users
 
   resources :incident_reports do
-      resources :versions, only: [:destroy] do
-        member do
-          get :diff, to: 'versions#diff'
-          patch :rollback, to: 'versions#rollback'
-        end
+
+    collection do
+      get :deleted
+      get :search
+    end
+
+    resources :versions, only: [:destroy] do
+      member do
+        get :diff, to: 'versions#diff'
+        patch :rollback, to: 'versions#rollback'
       end
+    end
   end
   resources :versions, only: [] do
     member do
