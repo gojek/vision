3d1811fe06bcc08b412839919e1f7dcb4c5f0cc9
adirizka7 <adiunity@gmail.com>
2018-09-03 13:41:46 +0700

    fix conflict

--- features/step_definitions/access_request_steps.rb
+++ features/step_definitions/access_request_steps.rb
@@ -26,8 +26,8 @@ Given(/^that access request has been approved on "([^"]*)"$/) do |time_approved|
 end
 Given /^an access request with employee name "([^"]*)" with "([^"]*)" as business justification$/ do |employee_name, business_justification|
   user = FactoryGirl.create(:user)
-  ar = FactoryGirl.create(:access_request, user: user, employee_name: employee_name, business_justification: business_justification)
-  version = ar.versions.first
+  @ar = FactoryGirl.create(:access_request, user: user, employee_name: employee_name, business_justification: business_justification)
+  version = @ar.versions.first
   version.whodunnit = user.id
   version.save!
 end
