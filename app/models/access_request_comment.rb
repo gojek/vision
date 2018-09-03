3bccd7c0045ad188053ed9d34eb3c70b82ed6cee
adirizka7 <adiunity@gmail.com>
2018-09-03 14:48:25 +0700

    fix indentation

--- app/models/access_request_comment.rb
+++ app/models/access_request_comment.rb
@@ -1,5 +1,5 @@
 class AccessRequestComment < ActiveRecord::Base
-	belongs_to :access_request
-	belongs_to :user
-	validates :body, presence:true
+  belongs_to :access_request
+  belongs_to :user
+  validates :body, presence:true
 end
