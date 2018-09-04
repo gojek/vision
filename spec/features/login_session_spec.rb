477a779570e0ad038642e4e90f3d078d258b24ad
adirizka7 <adiunity@gmail.com>
2018-09-04 14:06:30 +0700

    add rspec for one week user session

--- spec/features/login_session_spec.rb
+++ spec/features/login_session_spec.rb
@@ -1,21 +1,19 @@
 require "spec_helper"
 
-RSpec.describe "Login session timeout testing", :type => :feature do
-  it "User login session should be in two weeks" do
+RSpec.describe "Login session timeout testing", :type=>:feature do
+  it "User login session should be in one week" do
     user = FactoryGirl.create(:user)
     login_as(user)
     login_time = Time.current
-
-    #forward time to 14 days later
-    Timecop.freeze(Time.current + 13.days) do
+    #forward time to 6 days later  
+    Timecop.freeze(Time.current + 6.days) do
       #login_time is last time user login
       expect(user.timedout?(login_time)).to eq(false)
       visit new_change_request_path
       expect(page).to have_content("Change Request")
     end
-
-    #forward time to 30 days later
-    Timecop.freeze(Time.current + 30.days) do
+    #forward time to 8 days later
+    Timecop.freeze(Time.current + 8.days) do
       expect(user.timedout?(login_time)).to eq(true)
     end
   end
