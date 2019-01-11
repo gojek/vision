480c628be2a92e9ad9e2d1be0c87f9454a82b4f3
kianutama <kianutama.hudha@midtrans.com>
2019-01-11 17:38:43 +0700

    add file uploader

--- config/routes.rb
+++ config/routes.rb
@@ -63,7 +63,6 @@ Rails.application.routes.draw do
 
   get 'incident_reports/edit'
 
-
   get 'users/show'
 
   get 'users/index'
@@ -124,6 +123,7 @@ Rails.application.routes.draw do
 
     collection do
       get :search
+      post :import_from_csv
     end
   end
 
