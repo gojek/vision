21be07e8f451dc291429c3b19f9da7efe61ca650
kianutama <kianutama.hudha@midtrans.com>
2019-01-14 14:49:49 +0700

    google auth now only asking permission on first time login, and reduce contact permission to see and download only

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,7 @@
 # 'config/initializers/omniauth.rb'
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_API_KEY'], ENV['GOOGLE_API_SECRET'], {
-    prompt: 'consent',
-
-    access_type: 'offline',
-    scope: 'profile,email,calendar,https://www.google.com/m8/feeds/'
+  	access_type: 'offline',
+    scope: 'profile,email,calendar,https://www.googleapis.com/auth/contacts.readonly'
   }
 end
