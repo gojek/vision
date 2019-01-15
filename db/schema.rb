4ecf5478a3946422c8009449681ebf16f2395f84
Rizqy Faishal <rizqy.tanjung@midtrans.com>
2019-01-15 14:50:01 +0700

    temprorary update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190124034547) do
+ActiveRecord::Schema.define(version: 20190115065552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -99,6 +99,14 @@ ActiveRecord::Schema.define(version: 20190124034547) do
   end
 
   add_index "access_requests", ["user_id"], name: "index_access_requests_on_user_id", using: :btree
+
+  create_table "access_requests_associated_users", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "access_request_id"
+  end
+
+  add_index "access_requests_associated_users", ["access_request_id"], name: "index_access_requests_associated_users_on_access_request_id", using: :btree
+  add_index "access_requests_associated_users", ["user_id"], name: "index_access_requests_associated_users_on_user_id", using: :btree
 
   create_table "approvals", force: :cascade do |t|
     t.integer  "change_request_id"
@@ -304,13 +312,14 @@ ActiveRecord::Schema.define(version: 20190124034547) do
     t.datetime "created_at",                                   null: false
     t.datetime "updated_at",                                   null: false
     t.datetime "resolved_time"
-    t.decimal  "resolution_duration"
     t.decimal  "recovery_duration"
     t.boolean  "expected",                     default: false
     t.boolean  "has_further_action",           default: false
     t.text     "action_item"
     t.string   "action_item_status"
     t.datetime "action_item_done_time"
+    t.string   "visibility_type"
+    t.integer  "resolution_duration"
     t.integer  "time_to_acknowledge_duration"
   end
 
@@ -424,6 +433,8 @@ ActiveRecord::Schema.define(version: 20190124034547) do
   add_foreign_key "access_request_collaborators", "users"
   add_foreign_key "access_request_statuses", "access_requests"
   add_foreign_key "access_requests", "users"
+  add_foreign_key "access_requests_associated_users", "access_requests"
+  add_foreign_key "access_requests_associated_users", "users"
   add_foreign_key "approvals", "change_requests"
   add_foreign_key "approvals", "users"
   add_foreign_key "change_request_statuses", "change_requests"
