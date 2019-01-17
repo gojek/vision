f5e35940f5c7e490d45768262b5882587d91f6de
kianutama <kianutama.hudha@midtrans.com>
2019-01-17 17:25:57 +0700

    try creating the spec

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe ApplicationController, type: :controller do
+  context 'user access' do
+    let(:user) {FactoryGirl.create(:user)}
+    let(:waiting_user) {FactoryGirl.create(:waiting_user)}
+    let(:rejected_user) {FactoryGirl.create(:rejected_user)}
+    before :each do
+      controller.request.env['devise.mapping'] = Devise.mappings[:user]
+    end
+
+    it 'is sign_in rejected user' do
+      sign_in rejected_user
+      expect(response.status).to eq 200
+    end
+
+    it 'is sign_in waiting user' do
+      sign_in waiting_user
+      expect(response.status).to eq 200
+    end
+
+    it 'is sign_in approved user' do
+      sign_in user
+      expect(response.status).to eq 200
+    end
+
+  end
+end
