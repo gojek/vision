7e58e4b708ba6f4bca4dc911639c2fb47eeaa910
kianutama <kianutama.hudha@midtrans.com>
2019-01-17 09:33:29 +0700

    now valid email can login, and old email still can login

--- spec/controllers/access_request_controller_spec.rb
+++ spec/controllers/access_request_controller_spec.rb
@@ -1,36 +1,37 @@
 require 'spec_helper'
 
 describe AccessRequestsController, type: :controller do
-	context 'user access' do
-		let(:user) { FactoryGirl.create(:user)}
-		let(:access_request) { access_request = FactoryGirl.create(:access_request, user: user)}
-
-		before :each do
-			controller.request.env['devise.mapping'] = Devise.mappings[:user]
-			sign_in user
-		end	
-
-		describe 'GET #index' do
-      it 'populates an array of all access requests' do
-        get :index
-        expect(assigns(:access_requests)).to match_array([access_request])
-      end
-      it 'renders the :index view' do
-        get :index
-        expect(:response).to render_template :index
-      end
-
-      context "when download as csv" do
-        let(:csv_string)  {  access_request.to_csv }
-        let(:csv_options) { {filename: "access_requests.csv", disposition: 'attachment', type: 'text/csv'} }
-        let(:params) { {format: "csv", page: 1, per_page: 20}  }
-
-        it "should return current page when downloading an attachment" do
-          get :index, params
-          expect(response.header['Content-Type']).to eq('text/csv')
-        end
-      end
-
+  context 'user access' do
+    let(:user) {FactoryGirl.create(:user)}
+    let(:access_request) {access_request = FactoryGirl.create(:access_request, user: user)}
+    before :each do
+      controller.request.env['devise.mapping'] = Devise.mappings[:user]
+      sign_in user
     end
-	end
+
+    describe 'GET #new' do
+      it 'assigns a new Access Request to @access_request' do
+        get :new
+        expect(assigns(:access_request)).to be_a_new(AccessRequest)
+      end
+
+      it 'renders the :new template' do
+        get :new
+        expect(response).to render_template :new
+      end
+    end
+
+    describe 'GET #show' do
+      it 'assigns the requested access request to @access_request' do
+        get :show, id: access_request
+        expect(assigns(:access_request)).to eq access_request
+      end
+
+      it 'renders the :show template' do
+        get :show, id: access_request
+        expect(response).to render_template :show
+      end
+    end
+
+  end
 end
