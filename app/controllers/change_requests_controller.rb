67582db877df6335ef4376895a2fad0e89772b78
kianutama <kianutama.hudha@midtrans.com>
2019-01-18 17:37:48 +0700

    using active scope in approvers also

--- app/controllers/change_requests_controller.rb
+++ app/controllers/change_requests_controller.rb
@@ -83,7 +83,7 @@ class ChangeRequestsController < ApplicationController
     @current_implementers = []
     @current_testers = []
     @users = User.active.collect{|u| [u.name, u.id] }
-    @approvers = User.approvers.collect{|u| [u.name, u.id] if u.id != current_user.id && u.account_active? }
+    @approvers = User.approvers.active.collect{|u| [u.name, u.id] if u.id != current_user.id }
   end
 
   def edit
@@ -93,7 +93,7 @@ class ChangeRequestsController < ApplicationController
     @current_implementers = @change_request.implementers.collect{|u| u.id}
     @current_testers = @change_request.testers.collect{|u| u.id}
     @users = User.active.collect{|u| [u.name, u.id] }
-    @approvers = User.approvers.collect{|u| [u.name, u.id] if u.id != current_user.id && u.account_active? }
+    @approvers = User.approvers.active.collect{|u| [u.name, u.id] if u.id != current_user.id }
     @current_approvers = @change_request.approvals.collect(&:user_id)
   end
 
@@ -211,7 +211,7 @@ class ChangeRequestsController < ApplicationController
           @tags = ActsAsTaggableOn::Tag.all.collect(&:name)
           @current_tags = @change_request.tag_list
           @users = User.active.collect{|u| [u.name, u.id] }
-          @approvers = User.approvers.collect{|u| [u.name, u.id] if u.id != current_user.id && u.account_active? }
+          @approvers = User.approvers.active.collect{|u| [u.name, u.id] if u.id != current_user.id }
           format.html { render :edit }
           format.json { render json: @change_request.errors, status: :unprocessable_entity }
         end
@@ -279,7 +279,7 @@ class ChangeRequestsController < ApplicationController
     @users = User.active.collect{|u| [u.name, u.id] }
     @current_approvers = @old_change_request.approvals.collect(&:user_id)
     @change_request = @old_change_request.dup
-    @approvers = User.approvers.collect{|u| [u.name, u.id] if u.id != current_user.id && u.account_active? }
+    @approvers = User.approvers.active.collect{|u| [u.name, u.id] if u.id != current_user.id }
     # Clear certain fields
     @change_request.user = current_user
     @change_request.schedule_change_date = nil
@@ -299,7 +299,7 @@ class ChangeRequestsController < ApplicationController
     @users = User.active.collect{|u| [u.name, u.id] }
     @current_approvers = @old_change_request.approvals.collect(&:user_id)
     @change_request = @old_change_request.dup
-    @approvers = User.approvers.collect{|u| [u.name, u.id] if u.id != current_user.id && u.account_active? }
+    @approvers = User.approvers.active.collect{|u| [u.name, u.id] if u.id != current_user.id }
     # Clear certain fields
     @change_request.user = current_user
     @change_request.schedule_change_date = nil
