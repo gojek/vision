11f7fbff535c2fd861991738c7518509bcee79f5
Rizqy Faishal <rizqy.tanjung@midtrans.com>
2019-01-23 12:58:16 +0700

    separate implementation of register service

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,40 +1,21 @@
 # 'app/controllers/users/omniauth_callbacks_controller.rb'
+require 'register_service'
+
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
     is_new_user, @user = User.from_omniauth(request.env['omniauth.auth'])
     @user.token = request.env['omniauth.auth'][:credentials][:token]
     @user.refresh_token = request.env['omniauth.auth'][:credentials][:refresh_token]
     @user.expired_at = Time.at(request.env['omniauth.auth'][:credentials][:expires_at]).to_datetime
-    begin
-      @user.valid?
-      @user.save
-      if @user.persisted?
-        if is_new_user || @user.is_approved == User::NOT_YET_FILL_THE_FORM
-          sign_in @user
-          flash[:alert] = 'Please fill the form to requesting your access.'
-          redirect_to register_path
-        elsif @user.is_approved == User::WAITING_FOR_APPROVAL
-          flash[:alert] = 'Your account is not yet approved to open Vision'
-          redirect_to signin_path
-        elsif @user.is_approved == User::REJECTED
-          flash[:alert] = 'Sorry, your access request to Vision is rejected.'
-          redirect_to signin_path
-        else
-          sign_in_and_redirect @user, event: :authentication
-        end
+    is_signin, redirect_path, flash_messages, trigger_event = create_or_signin_user(is_new_user, @user)
+    if is_signin?
+      if trigger_event?
+        sign_in @user, event: :authentication
       else
-        if !@user.use_company_email?
-          err_message = 'Please use company email'
-        else
-          err_message = 'Authentication failed!'
-        end
-        redirect_to root_path, flash: { alert: err_message }
+        sign_in @user
       end
-    rescue ActiveRecord::ActiveRecordError
-      flash[:error] = 'An error occured. Please try again.'
-      redirect_to root_path
     end
-    
+    redirect_to redirect_path, flash: flash_messages    
   end
 
   def failure
