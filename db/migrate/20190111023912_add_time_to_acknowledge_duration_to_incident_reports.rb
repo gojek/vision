c8fe2c623f9a312a1bd7637aac26ddcbefbfdc77
Rizqy Faishal <rizqy.tanjung@midtrans.com>
2019-01-23 15:55:44 +0700

    update indentation spaces

--- db/migrate/20190111023912_add_time_to_acknowledge_duration_to_incident_reports.rb
+++ db/migrate/20190111023912_add_time_to_acknowledge_duration_to_incident_reports.rb
@@ -1,7 +1,7 @@
 class AddTimeToAcknowledgeDurationToIncidentReports < ActiveRecord::Migration
   def change
-  	change_table :incident_reports do |t|
-  		t.column :time_to_acknowledge_duration, :integer
-  	end
+    change_table :incident_reports do |t|
+      t.column :time_to_acknowledge_duration, :integer
+    end
   end
 end
