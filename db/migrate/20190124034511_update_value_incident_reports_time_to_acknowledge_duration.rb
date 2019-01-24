20ef089037557bbda89f58a7e44beff44a658654
Rizqy Faishal <rizqy.tanjung@midtrans.com>
2019-01-24 13:30:12 +0700

    convertion from seconds to minutes

--- db/migrate/20190124034511_update_value_incident_reports_time_to_acknowledge_duration.rb
+++ db/migrate/20190124034511_update_value_incident_reports_time_to_acknowledge_duration.rb
@@ -2,7 +2,7 @@ class UpdateValueIncidentReportsTimeToAcknowledgeDuration < ActiveRecord::Migrat
   def change
     execute <<-SQL
       UPDATE incident_reports 
-        SET time_to_acknowledge_duration = EXTRACT(epoch FROM acknowledge_time) - EXTRACT(epoch FROM detection_time);
+        SET time_to_acknowledge_duration = (EXTRACT(epoch FROM acknowledge_time) - EXTRACT(epoch FROM detection_time))/60;
     SQL
   end
 end
