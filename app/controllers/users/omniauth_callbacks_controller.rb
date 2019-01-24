3626e0de9633509cee3ee9918a719e5407d1ccfd
Rizqy Faishal <rizqy.tanjung@midtrans.com>
2019-01-24 17:36:18 +0700

    refactor code on handler registration and sign in

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,21 +1,23 @@
 # 'app/controllers/users/omniauth_callbacks_controller.rb'
-require 'register_service'
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
-    is_new_user, @user = User.from_omniauth(request.env['omniauth.auth'])
+    @user = User.from_omniauth(request.env['omniauth.auth'])
     @user.token = request.env['omniauth.auth'][:credentials][:token]
     @user.refresh_token = request.env['omniauth.auth'][:credentials][:refresh_token]
     @user.expired_at = Time.at(request.env['omniauth.auth'][:credentials][:expires_at]).to_datetime
-    is_signin, redirect_path, flash_messages, trigger_event = create_or_signin_user(is_new_user, @user)
-    if is_signin?
-      if trigger_event?
-        sign_in @user, event: :authentication
+    @user.valid?
+    @user.save
+		if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+    else
+      if !@user.use_company_email?
+        err_message = 'Please use company email'
       else
-        sign_in @user
+        err_message = 'Authentication failed!'
       end
+      redirect_to signin_path, flash: { alert: err_message }
     end
-    redirect_to redirect_path, flash: flash_messages    
   end
 
   def failure
