16f54913fed1234a3c84748ce270905a9158e451
kianutama <kianutama.hudha@midtrans.com>
2019-01-25 09:11:44 +0700

    create abstract function for the jiraize

--- db/migrate/20190122042905_add_action_to_change_request.rb
+++
@@ -1,6 +0,0 @@
-class AddActionToChangeRequest < ActiveRecord::Migration
-  def change
-    add_column :change_requests, :action_item, :text
-    add_column :change_requests, :action_item_status, :string
-  end
-end
