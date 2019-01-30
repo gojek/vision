205f143ba2014bfd1221d8cfd1937b1a8633b297
Rizqy Faishal <rizqy.tanjung@midtrans.com>
2019-01-30 13:00:24 +0700

    remove unused code about google contacts API

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_API_KEY'], ENV['GOOGLE_API_SECRET'], {
   	access_type: 'offline',
-    scope: 'profile,email,https://www.googleapis.com/auth/calendar.events,https://www.googleapis.com/auth/contacts.readonly'
+    scope: 'profile,email,https://www.googleapis.com/auth/calendar.events'
   }
 end
