05d6c0fcb76fa946ae2bd82a1270180581515323
Rizqy Faishal <rizqy.tanjung@midtrans.com>
2019-01-31 16:12:14 +0700

    update indentation tabs

--- app/models/approval.rb
+++ app/models/approval.rb
@@ -3,7 +3,7 @@ class Approval < ActiveRecord::Base
   belongs_to :user
 
   def self.setup_for_change_request(change_request, approver_ids)
-  	current_approver_ids = Approval.where(change_request_id: change_request.id).pluck(:user_id)
+    current_approver_ids = Approval.where(change_request_id: change_request.id).pluck(:user_id)
     deleted_approver_ids = current_approver_ids - approver_ids
     approver_ids = approver_ids - current_approver_ids
     if deleted_approver_ids.present?
@@ -12,8 +12,8 @@ class Approval < ActiveRecord::Base
     Approval.create(approver_ids.map { |approver_id| 
       { 
         user_id: approver_id, 
-    	change_request_id: change_request.id
+        change_request_id: change_request.id
       }
-   	})
+     })
   end
 end
