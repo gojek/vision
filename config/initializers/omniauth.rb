e9420bcc14d3651bf1e63416916f9390a3b460ab
Rizqy Faishal <rizqy.tanjung@midtrans.com>
2019-02-01 18:40:04 +0700

    restore consent

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,8 @@
 # 'config/initializers/omniauth.rb'
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_API_KEY'], ENV['GOOGLE_API_SECRET'], {
-  	access_type: 'offline',
+    access_type: 'offline',
+    prompt: 'consent',
     scope: 'profile,email,https://www.googleapis.com/auth/calendar.events'
   }
 end
