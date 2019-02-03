ab4075edc1ab459f2b2a76802bbf483541e76db2
Dwiyan Rahmanianto <mohamad.rahmanianto@midtrans.com>
2019-02-03 09:53:52 +0700

    rubocop autocorrect

--- lib/jira.rb
+++ lib/jira.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 class Jira
   include ActionView::Helpers::SanitizeHelper
 
   def initialize
     options = {
-      :username     => ENV['JIRA_USERNAME'],
-      :password     => ENV['JIRA_PASSWORD'],
-      :site         => ENV['JIRA_URL'],
-      :context_path => '',
-      :auth_type    => :basic
+      username: ENV['JIRA_USERNAME'],
+      password: ENV['JIRA_PASSWORD'],
+      site: ENV['JIRA_URL'],
+      context_path: '',
+      auth_type: :basic
     }
     @client = JIRA::Client.new(options)
   end
@@ -18,26 +20,26 @@ class Jira
     rescue JIRA::HTTPError => e
       return key
     end
-    
+
     summary = sanitize(issue.fields['summary'], tags: [])
     issue_type_icon = issue.fields['issuetype']['iconUrl']
     status_category = issue.fields['status']['statusCategory']
     color_name = status_category['colorName']
     name = status_category['name']
-    url = URI::join(ENV['JIRA_URL'],'/browse/',key)
+    url = URI.join(ENV['JIRA_URL'], '/browse/', key)
 
-    html  = "<span class='jira-button'>"
+    html = "<span class='jira-button'>"
     html << "  <a href='#{url}' target='_blank' data-toggle='popover' title='Summary' data-content='#{summary}'><img class='icon' src='#{issue_type_icon}'> #{key} </a>"
     html << "  <span class='jira-#{color_name.downcase}'>#{name}</span>"
-    html << "</span>"
+    html << '</span>'
   end
 
   def jiraize(text)
     return '' if text.nil?
     matches = text.scan(/([A-Z]+-\d+)/).map { |x| [x[0], get_issue(x[0])] }
-    for m in matches
+    matches.each do |m|
       text.gsub! m[0], m[1]
     end
-    return text
+    text
   end
 end
