113788422cfc91ddda50f627a1f08a20f0fe4665
kianutama <kianutama.hudha@midtrans.com>
2019-02-08 11:17:27 +0700

    add spec for the slack notif job

---
+++ spec/jobs/change_request_slack_new_job_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+require 'sucker_punch/testing/inline'
+
+RSpec.describe ChangeRequestSlackNewJob, type: :job do
+  let(:change_request) {FactoryGirl.create(:change_request) }
+  describe "perform async" do
+    it "send notification to slack immediately" do
+      ActiveJob::Base.queue_adapter = :test
+      expect {
+        ChangeRequestSlackNewJob.perform_later(change_request)
+      }.to have_enqueued_job.with(change_request)
+    end
+  end
+end
