bcb0c21ac1c48be1b2769be47c02b2d30f4e79cf
Rizqy Faishal <rizqy.tanjung@midtrans.com>
2019-02-13 17:25:42 +0700

    update changes

--- db/migrate/20190213063257_add_change_requests_tested_by.rb
+++
@@ -1,7 +0,0 @@
-class AddChangeRequestsTestedBy < ActiveRecord::Migration
-  def change
-    add_column :change_requests, :tested_by_qa, :boolean, default: false
-    add_column :change_requests, :tested_by_dev, :boolean, default: false
-    add_column :change_requests, :tested_by_dev_ops, :boolean, default: false
-  end
-end
