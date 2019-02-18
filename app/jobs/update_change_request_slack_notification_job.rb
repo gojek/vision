9128753cb8a072d9274344d55d224d326134581c
kianutama <kianutama.hudha@midtrans.com>
2019-02-18 11:33:55 +0700

    refactor class name

--- app/jobs/update_change_request_slack_notification_job.rb
+++ app/jobs/update_change_request_slack_notification_job.rb
@@ -1,6 +1,7 @@
 require 'slack_notif.rb'
 
-class ChangeRequestSlackUpdateJob < ActiveJob::Base
+# class ChangeRequestSlackUpdateJob < ActiveJob::Base
+class UpdateChangeRequestSlackNotificationJob < ActiveJob::Base
   include SuckerPunch::Job 
 
   def perform(change_request)
--- spec/jobs/update_change_request_slack_notification_job_spec.rb
+++ spec/jobs/update_change_request_slack_notification_job_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 require 'sucker_punch/testing/inline'
 
-RSpec.describe ChangeRequestSlackNewJob, type: :job do
+RSpec.describe UpdateChangeRequestSlackNotificationJob, type: :job do
   let(:change_request) {FactoryGirl.create(:change_request) }
   describe "perform async" do
     it "send notification to slack immediately" do
       ActiveJob::Base.queue_adapter = :test
       expect {
-        ChangeRequestSlackNewJob.perform_later(change_request)
+        UpdateChangeRequestSlackNotificationJob.perform_later(change_request)
       }.to have_enqueued_job.with(change_request)
     end
   end
