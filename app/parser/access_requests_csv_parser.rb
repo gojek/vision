4e9b9600abe267aba00239d668f44872f677b701
kianutama <kianutama.hudha@midtrans.com>
2019-02-18 18:04:53 +0700

    fix code styling

--- app/parser/access_requests_csv_parser.rb
+++ app/parser/access_requests_csv_parser.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 require 'access_request_csv_parser.rb'
 
 class AccessRequestsCsvParser
   def self.process_csv(file, current_user)
     @valid = []
     @invalid = []
-    CSV.foreach(file.path,headers: true, col_sep: ",") do |row|
+    CSV.foreach(file.path, headers: true, col_sep: ',') do |row|
       item_parser = AccessRequestCsvParser.parse(row.to_h, current_user)
       ar = item_parser.generate_access_request
       if ar.invalid? || item_parser.item_valid?
@@ -13,6 +15,6 @@ class AccessRequestsCsvParser
         @valid << ar
       end
     end
-    return @valid, @invalid
+    [@valid, @invalid]
   end
 end
