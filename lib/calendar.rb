f22ede6453ab92e30c7e186074ad305ddb78811d
Rizqy Faishal <rizqy.tanjung@midtrans.com>
2019-02-22 17:28:32 +0700

    apply rubocop

--- lib/calendar.rb
+++ lib/calendar.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'google/apis/calendar_v3'
 require 'user_credential.rb'
 
@@ -9,13 +11,17 @@ class Calendar
     set_event(user, event, change_request.google_event_id)
   end
 
+  private
+
   def build_event_from_cr(change_request)
     users = [change_request.user] + change_request.collaborators + change_request.implementers
     event = Google::Apis::CalendarV3::Event.new
     event.summary = change_request.change_summary
     event.description = "CR: #{change_request_url(change_request)}\nPIC: #{change_request.requestor_name}"
     event.location = ''
-    event.start = Google::Apis::CalendarV3::EventDateTime.new(date_time: change_request.schedule_change_date.to_datetime)
+    event.start = Google::Apis::CalendarV3::EventDateTime.new(
+      date_time: change_request.schedule_change_date.to_datetime
+    )
     event.end = Google::Apis::CalendarV3::EventDateTime.new(date_time: change_request.planned_completion.to_datetime)
     event.attendees = users.map do |user|
       Google::Apis::CalendarV3::EventAttendee.new(email: user.email)
@@ -23,19 +29,24 @@ class Calendar
     event
   end
 
-  def set_event(user, event, event_id=nil)
-    service = Google::Apis::CalendarV3::CalendarService.new
-    user_credential = UserCredential.new(user.refresh_token, 'https://www.googleapis.com/auth/calendar.events,https://www.googleapis.com/auth/calendar')
-    service.authorization = user_credential.get_credentials
+  def set_event(user, event, event_id = nil)
     calendar_id = ENV['DEPLOY_CALENDAR_ID']
-    begin
-      if event_id.nil?
-        return true, service.insert_event(calendar_id, event, send_notifications: true)
-      else
-        return true, service.update_event(calendar_id, event_id, event, send_notifications: true)
-      end
-    rescue Google::Apis::ClientError => error
-      return false, { error_message: error.to_s }
+    event = if event_id.nil?
+              service(user).insert_event(calendar_id, event, send_notifications: true)
+            else
+              service(user).update_event(calendar_id, event_id, event, send_notifications: true)
+            end
+    [true, event]
+  rescue Google::Apis::ClientError => error
+    [false, { error_message: error.to_s }]
+  end
+
+  def service(user)
+    if @service.nil?
+      @service = Google::Apis::CalendarV3::CalendarService.new
+      user_credential = UserCredential.new(user.refresh_token)
+      @service.authorization = user_credential.authorize
     end
+    @service
   end
 end
