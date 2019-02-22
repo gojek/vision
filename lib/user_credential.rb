f38a464a89cbae2e7a29e188e3a8a34fb91fc96d
Rizqy Faishal <rizqy.tanjung@midtrans.com>
2019-02-22 17:28:32 +0700

    apply rubocop

--- lib/user_credential.rb
+++ lib/user_credential.rb
@@ -1,19 +1,22 @@
-require "googleauth"
+# frozen_string_literal: true
+
+require 'googleauth'
 
 class UserCredential
-  def initialize(refresh_token, scope)
+  def initialize(refresh_token)
     @refresh_token = refresh_token
-    @scope = scope
+    @scope = 'https://www.googleapis.com/auth/calendar.events,https://www.googleapis.com/auth/calendar'
   end
 
-  def get_credentials
+  def authorize
     credentials = Google::Auth::UserRefreshCredentials.new(
       client_id: ENV['GOOGLE_API_KEY'],
       client_secret: ENV['GOOGLE_API_SECRET'],
       scope: @scope,
-      additional_parameters: { "access_type" => "offline" })
+      additional_parameters: { 'access_type' => 'offline' }
+    )
     credentials.refresh_token = @refresh_token
     credentials.fetch_access_token!
-    return credentials
+    credentials
   end
 end
