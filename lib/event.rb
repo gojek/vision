714c1efabb4f9df9da1c7639978a984ac4dffe8a
Rizqy Faishal <rizqy.tanjung@midtrans.com>
2019-02-25 18:24:46 +0700

    refactor

---
+++ lib/event.rb
@@ -0,0 +1,46 @@
+require 'google/apis/calendar_v3'
+
+class Event < Google::Apis::CalendarV3::Event
+  attr_writer :saved
+  attr_writer :error_messages
+
+  def initialize
+    @saved = false
+    @error_messages = nil
+  end
+
+  def self.build_event_from_change_request(change_request)
+    routes = Rails.application.routes.url_helpers
+    event = Event.new
+    users = [change_request.user] + change_request.collaborators + change_request.implementers
+    event.id ||= change_request.google_event_id
+    event.summary = change_request.change_summary
+    event.description = "CR: #{routes.change_request_url(change_request)}\nPIC: #{change_request.requestor_name}"
+    event.location = ''
+    event.start = Google::Apis::CalendarV3::EventDateTime.new(
+      date_time: change_request.schedule_change_date.to_datetime
+    )
+    event.end = Google::Apis::CalendarV3::EventDateTime.new(date_time: change_request.planned_completion.to_datetime)
+    event.attendees = users.map do |user|
+      Google::Apis::CalendarV3::EventAttendee.new(email: user.email)
+    end
+    event
+  end
+
+  def success?
+    @saved
+  end
+ 
+  def saved=(saved)
+    @saved = saved
+  end
+
+  def error_messages=(error_messages)
+    @error_messages = error_messages
+  end
+
+  def error_messages
+    @error_messages
+  end
+
+end
