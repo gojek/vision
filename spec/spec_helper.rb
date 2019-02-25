eb590906735301148c6fb0c05298b82974f4b4af
kianutama <kianutama.hudha@midtrans.com>
2019-02-25 16:31:50 +0700

    fix when the key is not found

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -154,7 +154,7 @@ end
 
 def generate_jira_jql_link(issues)
   jql = issues.map { |issue| URI.encode_www_form("issueKey"=>issue) }.join(" OR ")
-  stub_request(:get, "https://veritrans.atlassian.net/rest/api/2/search?fields=summary,status,issuetype&jql="+jql)
+  stub_request(:get, "https://veritrans.atlassian.net/rest/api/2/search?fields=summary,status,issuetype&jql="+jql+"&validateQuery=false")
       .to_return(
         status: 200, 
         body: '{"issues":[' + issues.map{ |issue| '
