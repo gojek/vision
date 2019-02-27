a8bdbcbda86f07fda0f0489d414b79feefe0be72
kianutama <kianutama.hudha@midtrans.com>
2019-02-27 13:41:17 +0700

    edit redundancy

--- spec/jobs/approval_change_request_slack_notification_job_spec.rb
+++ spec/jobs/approval_change_request_slack_notification_job_spec.rb
@@ -5,11 +5,10 @@ RSpec.describe ApprovalChangeRequestSlackNotificationJob, type: :job do
   let(:change_request) {FactoryGirl.create(:change_request) }
   describe "perform async" do
     it "send notification to slack immediately" do
-    	accept_note = "accepted"
     	approval = Approval.where(change_request_id: change_request.id, user_id: change_request.approvals.first.user_id).first
     	approval.approve = true
       approval.approval_date = Time.current
-      approval.notes = accept_note
+      approval.notes = "accepted"
       approval.save!
 
       ActiveJob::Base.queue_adapter = :test
@@ -19,18 +18,17 @@ RSpec.describe ApprovalChangeRequestSlackNotificationJob, type: :job do
     end
 
     it "request stub for notification to slack" do
-    	accept_note = "accepted"
     	approval = Approval.where(change_request_id: change_request.id, user_id: change_request.approvals.first.user_id).first
     	approval.approve = true
       approval.approval_date = Time.current
-      approval.notes = accept_note
+      approval.notes = "accepted"
       approval.save!
 
       stub = stub_request(:post, "https://slack.com/api/chat.postMessage")
         .to_return(status: 200, body: '{"ok": true}', headers: {})
       ActiveJob::Base.queue_adapter = :test
       ApprovalChangeRequestSlackNotificationJob.perform_async(change_request, approval)
-      expect(stub).to have_been_requested.times(4)
+      expect(stub).to have_been_requested.times(5)
     end
   end
 end
