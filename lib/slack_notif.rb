941ba90842e88d35c8dea806e285cafed5093d37
kianutama <kianutama.hudha@midtrans.com>
2019-02-27 12:49:56 +0700

    refactor function names

--- lib/slack_notif.rb
+++ lib/slack_notif.rb
@@ -31,7 +31,7 @@ class SlackNotif
     link = change_request_url(change_request)
     approvers = change_request.approvals.collect(&:user)
     approver_message = "#{type.humanize} <#{link}|change request> needs your approvals"
-    notify_users(approvers, approver_message, attachment)
+    notify_approvers(approvers, approver_message, attachment)
     associated_users = change_request.associated_users.to_a
     approvers.each { |approver| associated_users.delete(approver) }
     general_message = "<#{link}|Change request> has been #{type}"
@@ -44,7 +44,7 @@ class SlackNotif
     link = access_request_url(access_request)
     approvers = access_request.approvals.collect(&:user)
     approver_message = "#{type.humanize} <#{link}|access_request> needs your approvals"
-    notify_users(approvers, approver_message, attachment)
+    notify_approvers(approvers, approver_message, attachment)
   end
 
   def notify_approval_status_cr(change_request, approval)
@@ -112,8 +112,7 @@ class SlackNotif
   end
 
   private
-
-  def notify_users(users, message, attachment)
+  def notify_approvers(users, message, attachment)
     actionable_attachment = @attachment_builder.wrap_approver_actions(attachment)
     @slack_client.message_users(users, message, attachment)
   end
