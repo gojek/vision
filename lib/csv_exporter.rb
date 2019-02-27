ba0771aaf6562bdeec66a6f833bfdd1e327aa07b
Rizqy Faishal <rizqy.tanjung@midtrans.com>
2019-02-27 18:05:48 +0700

    fix rubocop formatting

--- lib/csv_exporter.rb
+++ lib/csv_exporter.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class CSVExporter
   def self.export_from_active_records(records)
-    enumerator = Enumerator.new do |lines|
+    Enumerator.new do |lines|
       lines << records.model.to_comma_headers.to_csv
       records.find_each do |record|
         lines << record.to_comma.to_csv
