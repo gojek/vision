10369e2fe3889bd88f57e91079d789b68da4c23c
kianutama <kianutama.hudha@midtrans.com>
2019-03-06 17:28:30 +0700

    create jira helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -151,28 +151,3 @@ def login_as(user)
   visit("/users/auth/google_oauth2")
   OmniAuth.config.test_mode = false
 end
-
-def generate_jira_jql_link(issues)
-  jql = issues.map { |issue| URI.encode_www_form("issueKey"=>issue) }.join(" OR ")
-  stub_request(:get, "https://veritrans.atlassian.net/rest/api/2/search?fields=summary,status,issuetype&jql="+jql+"&validateQuery=false")
-      .to_return(
-        status: 200, 
-        body: '{"issues":[' + issues.map{ |issue| '
-                {
-                  "id":"1234",
-                  "key":"'+issue+'",
-                  "fields":{
-                    "summary":"summary",
-                    "issuetype":{
-                      "iconUrl":"https://veritrans.atlassian.net/images/icons/statuses/generic.png"
-                    },
-                    "status":{
-                      "statusCategory":{
-                        "colorName":"yellow",
-                        "name":"In Progress"
-                      }
-                    }
-                  }
-            }'}.join(",\n") + ']}', 
-        headers: {})
-end
