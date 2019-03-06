a71226735bd7f85cf2c849260f7e012602d2abe3
kianutama <kianutama.hudha@midtrans.com>
2019-03-06 10:30:20 +0700

    fix indentation

--- spec/slack_helpers.rb
+++ spec/slack_helpers.rb
@@ -1,14 +1,24 @@
+# frozen_string_literal: true
+
 module SlackHelpers
+  def user_success_stub
+    stub_request(:post, 'https://slack.com/api/users.lookupByEmail')
+      .with(body: { 'email' => 'patrick@veritrans.co.id', 'token' => nil })
+      .to_return(status: 200,
+                 body: '{
+                  "ok": true,
+                  "user": {
+                    "name":"patrick.star",
+                    "profile":{
+                      "email":"patrick@veritrans.co.id"
+                    }
+                  }
+                }', headers: {})
+  end
 
-	def user_success_stub
-		stub_request(:post, "https://slack.com/api/users.lookupByEmail").
-			with(:body => {"email"=>"patrick@veritrans.co.id", "token"=>nil}).
-			to_return(status: 200, body: '{"ok": true, "user": {"name":"patrick.star", "profile":{"email":"patrick@veritrans.co.id"}} }', headers: {})
-	end
-
-	def user_failed_stub
-		stub_request(:post, "https://slack.com/api/users.lookupByEmail").
-			with(:body => {"email"=>"dummy@dummy.com", "token"=>nil}).
-			to_return(status: 200, body: '{"ok": false, "error":"users_not_found"}', headers: {})
-	end
+  def user_failed_stub
+    stub_request(:post, 'https://slack.com/api/users.lookupByEmail')
+      .with(body: { 'email' => 'dummy@dummy.com', 'token' => nil })
+      .to_return(status: 200, body: '{"ok": false, "error":"users_not_found"}', headers: {})
+  end
 end
