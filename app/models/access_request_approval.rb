807ec739575745b859a2430f1ec30a7c45d26213
Rizqy Faishal <rizqy.tanjung@midtrans.com>
2019-03-11 14:48:07 +0700

    update changes

--- app/models/access_request_approval.rb
+++ app/models/access_request_approval.rb
@@ -7,4 +7,21 @@ class AccessRequestApproval < ActiveRecord::Base
   def decided?
     return !self.approved.nil?
   end
+
+  def self.setup_for_access_request(access_request, approver_ids)
+    current_approver_ids = access_request.persisted? ? 
+      AccessRequestApproval.where(access_request_id: access_request.id).pluck(:user_id) : []
+    deleted_approver_ids = current_approver_ids - approver_ids
+    approver_ids = approver_ids - current_approver_ids
+
+    AccessRequestApproval.where(access_request_id: access_request.id, user_id: deleted_approver_ids).destroy_all
+    AccessRequestApproval.create(
+      approver_ids.map do |approver_id|
+        {
+          access_request_id: access_request.id,
+          user_id: approver_id
+        }
+      end
+    )
+  end
 end
