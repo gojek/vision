2c3e32f02e4916fdb348ca8ab7110c8ddd6ddc0c
kianutama <kianutama.hudha@midtrans.com>
2019-03-13 15:37:54 +0700

    refactor code

--- spec/factories/access_request_comments.rb
+++ spec/factories/access_request_comments.rb
@@ -3,9 +3,5 @@
 FactoryGirl.define do
   factory :access_request_comment do
     body Faker::Lorem.sentence(5)
-
-    factory :ar_comment_with_mention do
-      body "@" + Faker::Internet.user_name(Faker::Name.name, %w(. _ .)) + " @" + Faker::Internet.user_name(Faker::Name.name, %w(. _ .))
-    end
   end
 end
