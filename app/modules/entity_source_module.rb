880cccc45b2cb79748539d9c44e33a6f2b1ed847
Rizqy Faishal Tanjung <rizqy.tanjung@midtrans.com>
2019-03-18 19:48:38 +0700

    apply rubocop

--- app/modules/entity_source_module.rb
+++ app/modules/entity_source_module.rb
@@ -1,6 +1,8 @@
-module EntitySourceModule extend ActiveSupport::Concern
+# frozen_string_literal: true
 
-  ENTITY_SOURCES = (ENV['ENTITY_SOURCES'] || 'Midtrans').split(",").map!(&:capitalize)
+module EntitySourceModule
+  extend ActiveSupport::Concern
+  ENTITY_SOURCES = (ENV['ENTITY_SOURCES'] || 'Midtrans').split(',').map!(&:capitalize)
 
   included do
     validates :entity_source, presence: true
