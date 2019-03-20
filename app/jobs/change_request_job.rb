76b99b00925bd6ec834c3ace5bdf782210c88b2f
Rizqy Faishal Tanjung <rizqy.tanjung@midtrans.com>
2019-03-20 18:14:23 +0700

    fix change request job

--- app/jobs/change_request_job.rb
+++ app/jobs/change_request_job.rb
@@ -4,13 +4,13 @@ class ChangeRequestJob
   include SuckerPunch::Job 
 
   def perform(cr_ids, email)
-    csv_string = CSV.generate do |csv|
-      csv << ChangeRequest.attribute_names
-      ChangeRequest.where(id: cr_ids).where.not(aasm_state: 'draft').order("created_at DESC").each do |cr|
-        csv << cr.attributes.values
-  	  end
-	  end
     ActiveRecord::Base.connection_pool.with_connection do
+      csv_string = CSV.generate do |csv|
+        csv << ChangeRequest.attribute_names
+        ChangeRequest.where(id: cr_ids).where.not(aasm_state: 'draft').order("created_at DESC").each do |cr|
+          csv << cr.attributes.values
+        end
+      end
 		  ChangeRequestMailer.send_csv(csv_string, email).deliver_now
     end
   end
