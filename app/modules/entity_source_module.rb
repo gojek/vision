2db3d63a7749fb86cda7518780e072b0d750430e
Rizqy Faishal Tanjung <rizqy.tanjung@midtrans.com>
2019-03-20 15:29:52 +0700

    dont forget to apply rubocop

--- app/modules/entity_source_module.rb
+++ app/modules/entity_source_module.rb
@@ -7,8 +7,7 @@ module EntitySourceModule
   included do
     validates :entity_source, presence: true
     comma :entity_source do
-      entity_source "Entity source"
+      entity_source 'Entity source'
     end
   end
-
 end
