ad4cb7e20834c13173f9010cf7737c2b8c72a0f9
Rizqy Faishal Tanjung <rizqy.tanjung@midtrans.com>
2019-03-20 17:31:37 +0700

    fix some code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require 'simplecov'
 SimpleCov.start 'rails'
 ENV["RAILS_ENV"] ||= 'test'
 ENV["VALID_EMAIL"] ||= 'midtrans.com,veritrans.co.id,go-jek.com'
+ENV['VISION_EMAIL'] ||= 'vision@midtrans.com'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'webmock/rspec'
