c56ff986ca9fc08230d17321b57329c0e753bb6e
Rori Kurniadi <rori.development@gmail.com>
2021-08-28 18:25:19 +0700

    upgrade to ruby 2.5.8

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :comment do
-    body Faker::Lorem.sentence(5)
+    body Faker::Lorem.sentence(word_count: 5)
 
     factory :comment_with_mention do
-      body "@" + Faker::Internet.user_name(Faker::Name.name, %w(. _ .)) + " @" + Faker::Internet.user_name(Faker::Name.name, %w(. _ .))
+      body "@" + Faker::Internet.user_name(specifier: Faker::Name.name, separators: %w(. _ .)) + " @" + Faker::Internet.user_name(specifier: Faker::Name.name, separators: %w(. _ .))
     end
   end
 
