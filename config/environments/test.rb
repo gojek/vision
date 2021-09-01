3207534c3ccfa0d065445bae1cd97c46fed99c46
Rori Kurniadi <rori.development@gmail.com>
2021-09-02 02:49:54 +0700

    upgrade to 5.2

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,14 +1,14 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+
   #Setting for host domain for route
   Rails.application.routes.default_url_options = { host: 'localhost', port: 3000 }
+
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
   # and recreated between test runs. Don't rely on the data there!
   config.cache_classes = true
-
-  config.active_record.belongs_to_required_by_default = false
 
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
@@ -18,7 +18,7 @@ Rails.application.configure do
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
   config.public_file_server.headers = {
-    'Cache-Control' => 'public, max-age=3600'
+    'Cache-Control' => "public, max-age=#{1.hour.to_i}"
   }
 
   # Show full error reports and disable caching.
@@ -30,6 +30,10 @@ Rails.application.configure do
 
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
+
+  # Store uploaded files on the local file system in a temporary directory
+  config.active_storage.service = :test
+
   config.action_mailer.perform_caching = false
 
   # Tell Action Mailer not to deliver emails to the real world.
@@ -40,7 +44,10 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
+  config.active_record.belongs_to_required_by_default = false
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
   config.action_mailer.default_url_options = {:host => "localhost:3000"}
 end
