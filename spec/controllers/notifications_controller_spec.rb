408e29dcbf738a86c38ef2755b144c507044cc4b
Rori Kurniadi <rori.development@gmail.com>
2021-09-02 03:03:53 +0700

    add params to parse data

--- spec/controllers/notifications_controller_spec.rb
+++ spec/controllers/notifications_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe NotificationsController, type: :controller do
   let(:user) { FactoryBot.create(:user) }
 
   before(:each) do
-    controller.request.env["devise.mapping"] = Devise.mappings[:user]
+    @request.env["devise.mapping"] = Devise.mappings[:user]
     sign_in user
     request.env["HTTP_REFERER"] = "vt-vision.com"
   end
@@ -30,25 +30,25 @@ RSpec.describe NotificationsController, type: :controller do
     end
 
     it 'should assign cr to @active' do
-      get :index, type: 'cr'
+      get :index, params: { type: 'cr' }
       expect(assigns(:active)).to eq 'cr'
     end
 
     it 'should assign ir to @active' do
-      get :index, type: 'ir'
+      get :index, params: { type: 'ir' }
       expect(assigns(:active)).to eq 'ir'
     end
 
     it 'should assign true to @have_cr_notif if user have any change request notification' do
       cr = FactoryBot.create(:change_request)
       notification = FactoryBot.create(:notification, user: user, change_request: cr, message: 'new_cr')
-      get :index, type: 'cr'
+      get :index, params: { type: 'cr' }
       expect(assigns(:notifications).count).to eq 1
       expect(assigns(:have_cr_notif)).to eq true
     end
 
     it 'should assign false to @have_cr_notif if user have no change request notification' do
-      get :index, type: 'cr'
+      get :index, params: { type: 'cr' }
       expect(assigns(:notifications).count).to eq 0
       expect(assigns(:have_cr_notif)).to eq false
     end
@@ -56,13 +56,13 @@ RSpec.describe NotificationsController, type: :controller do
     it 'should assign true to @have_ir_notif if user have any incident report notification' do
       ir = FactoryBot.create(:incident_report)
       notification = FactoryBot.create(:notification, user: user, incident_report: ir, message: 'new_ir')
-      get :index, type: 'ir'
+      get :index, params: { type: 'ir' }
       expect(assigns(:notifications).count).to eq 1
       expect(assigns(:have_ir_notif)).to eq true
     end
 
     it 'should assign false to @have_ir_notif if user have no incident report notification' do
-      get :index, type: 'ir'
+      get :index, params: { type: 'ir' }
       expect(assigns(:notifications).count).to eq 0
       expect(assigns(:have_ir_notif)).to eq false
     end
