6286633d8eb4674e9123c2f0ac6f4c58518e3c0b
Rori Kurniadi <rori.development@gmail.com>
2021-09-11 17:35:21 +0700

    add pg search schema migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_01_162940) do
+ActiveRecord::Schema.define(version: 2021_09_11_102603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -319,6 +319,15 @@ ActiveRecord::Schema.define(version: 2021_09_01_162940) do
     t.index ["change_request_id"], name: "index_notifications_on_change_request_id"
     t.index ["incident_report_id"], name: "index_notifications_on_incident_report_id"
     t.index ["user_id"], name: "index_notifications_on_user_id"
+  end
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
   end
 
   create_table "read_marks", force: :cascade do |t|
