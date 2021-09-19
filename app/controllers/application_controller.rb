27b27f5d687d6c6b1d91bc120ee57dcb0a3e53c1
Rori Kurniadi <rori.development@gmail.com>
2021-09-19 22:03:03 +0700

    rollback papertrail

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   before_action :check_user
   before_action :check_pending_user
-  before_action :set_paper_trail_whodunnit
+  # before_action :set_paper_trail_whodunnit
   require 'notifier.rb' # Omg
 
   def check_user
